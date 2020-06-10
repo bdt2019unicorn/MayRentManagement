66f6f8eb48e5a66c5131996f7785a50f44a55a1c
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-10 16:34:35 +1200

    couple more database things to get done
    Former-commit-id: a3d4a0a54d7a3e6dde3be07a26456acf4696914c

--- server/server.php
+++ server/server.php
@@ -1,38 +1,9 @@
 <?php 
-    $excel = json_decode($_POST['excel']); 
-
-    foreach ($excel as $tenant) 
-    {
-        $sql = "INSERT INTO `tenant`"; 
-        $columns = "("; 
-        $values = "VALUES ("; 
-        foreach ($tenant as $key => $value) 
-        {
-            if($key!="Tenant_ID")
-            {
-                if($key=="Apartment")
-                {
-                    $columns.='appartment_id,'; 
-                }
-            }
-        }
-
-        
-    }
-
-    Connect::GetData("\nI am testing my connection and I think it may fail\n"); 
-
     require_once(realpath(dirname(__DIR__)."\\vendor\\autoload.php")); 
     use Dotenv\Dotenv; 
     $dotenv = Dotenv::createImmutable(dirname(__DIR__)); 
     $dotenv->load(); 
 
-
-
-
-
-
-    // require_once realpath(__DIR__.)
     class Connect
     {
         private static function Connection()
@@ -40,50 +11,168 @@
             $servername = $_ENV['SERVERNAME'];
             $username = $_ENV['USERNAME'];
             $password = $_ENV['PASSWORD'];
+            $dbname = $_ENV['DBNAME']; 
+
+            $connection = null; 
             
             try 
             {
-                $connection = new mysqli($servername, $username, $password);
+                $connection = new mysqli($servername, $username, $password,$dbname);
                 if ($connection -> connect_errno) 
                 {
                     throw new Exception("\nUnable to connect to the database\n"); 
                 }
-                return $connection;  
             }
-            catch(Throwable $e)
+            catch(Throwable $t)
             {
-                return null;
+                $connection = null; 
             }
+            ob_end_clean();
+            return $connection; 
         }
 
-        public static function GetData($sql)
+        public static function GetData($sql, $get_id=false)
         {
             $connection = Connect::Connection(); 
-            ob_end_clean();
             if($connection==null)
             {
                 return null; 
             }
-            else 
-            {
-                echo "\n It is really bad \n"; 
 
-                $connection->close(); 
-                // mysqli_close($connection); 
-                return null; 
+            $data = $connection->query($sql); 
+            $result =null; 
+            if(is_bool($data))
+            {
+                if($data&&$get_id)
+                {
+                    $result = $connection->insert_id; 
+                }
+                else 
+                {
+                    $result = $data; 
+                }
             }
+            try 
+            {
+                $result = (!$result)?$data->fetch_all(MYSQLI_ASSOC):$result; 
+            }
+            catch(Throwable $t){}
+            $connection->close(); 
+            return $result; 
         }
 
-
-        public static function GetId($column, $value, $table)
+        public static function ExecTransaction($sql)
         {
-            
-            // $sql = "SELECT * FROM `". $table."` WHERE ".$column." = 
-            // $data = Connect::GetData(); 
+            $connection = Connect::Connection(); 
+            if($connection==null)
+            {
+                return null; 
+            }
+            $result = false; 
+            try 
+            {
+                $connection->autocommit(false);
+
+                foreach ($sql as $query) 
+                {
+                    echo "\n"; 
+                    echo $query; 
+                    echo"\n"; 
+                    $result = $connection->query($query); 
+                    if(!$result)
+                    {
+                        throw new Exception("\n Database error\n"); 
+                    }
+                }
+                $connection->commit(); 
+                $result = true; 
+            }
+            catch(Throwable $t)
+            {
+                echo $t->getMessage(); 
+                $connection->rollback(); 
+            }
+
+            $connection->close(); 
+            return $result; 
+        }
+
+        public static function GetId($column, $value, $table,$id_field='id')
+        {
+            $value = str_replace("'","\'",$value); 
+            $sql = "SELECT * FROM `". $table."` WHERE ".$column." = '".$value."'"; 
+            $data = Connect::GetData($sql);
+            if(is_bool($data))
+            {
+                return null; 
+            }
+            if(count($data)>0)
+            {
+                return $data[0][$id_field]; 
+            } 
+            else 
+            {
+                $sql = "INSERT INTO ".$table."(".$column.") VALUES ('".$value."'); ";
+                return Connect::GetData($sql, true); 
+            }
         }
     }
 
+    $excel = json_decode($_POST['excel']); 
 
+    foreach ($excel as $tenant) 
+    {
+        $sql = "INSERT INTO `tenant`"; 
+        global $columns; 
+        global $values; 
+        $columns = "("; 
+        $values = "VALUES ("; 
+
+        function StringAddition($key, $value)
+        {
+            global $columns; 
+            global $values; 
+            $column = str_replace(' ','_',$key);
+            $value = str_replace("'","\'",$value); 
+
+            $columns.=$column.',';
+            $values.= "'".$value."',";  
+        }
+
+        foreach ($tenant as $key => $value) 
+        {
+            if($key!="Tenant_ID")
+            {
+                if($key=="Apartment")
+                {
+                    StringAddition('appartment_id', Connect::GetId('appartment_id',$value, 'tenant')); 
+                }
+                else 
+                {
+                    StringAddition($key, $value); 
+                }
+            }
+        }
+
+        
+    }
+
+    // $data_result = Connect::GetData("\n insert into `buildings`(name) values ('test J'); \n", true); 
+    // print_r($data_result); 
+
+    // $sqls = array
+    // (
+    //     "insert into `buildings`(name) values ('test 9'); ",
+    //     "insert into `buildings`(name1) values ('test 10'); ",
+    //     "insert into `buildings`(name) values ('test J'); "
+    // ); 
+    // $result = Connect::ExecTransaction($sqls); 
+    // echo $result; 
+
+
+//     $str = "Hello world";
+// $i = 4;
+// echo substr_replace($str, '', $i, 1);
 
 
 
