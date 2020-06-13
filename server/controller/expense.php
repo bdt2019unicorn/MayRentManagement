467f6569735bbf42b48b63ff17f738309ff19bb0
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-13 16:20:21 +1200

    finish testing, put excel template and start doing the design work
    Former-commit-id: b242e454e8216caf4e78d7cfe6ed06e6ef3d3468

--- server/controller/expense.php
+++ server/controller/expense.php
@@ -1,6 +1,5 @@
 <?php 
-    require_once("../helper/connect.php"); 
-
+    require_once("main_controller.php"); 
     $table = "expense"; 
     $date_collumns = ["start_period", "end_period", "payment_date"]; 
     $exclude_columns = []; 
@@ -20,57 +19,7 @@
         )
     );
     $year_number = "20"; 
+    $comma = ["Amount"]; 
 
-    $excel = json_decode($_POST['excel']); 
-    $queries = []; 
-
-    foreach ($excel as $row) 
-    {
-        $sql = "INSERT INTO `".$table."`"; 
-        global $columns; 
-        global $values; 
-        $columns = "("; 
-        $values = "VALUES ("; 
-
-        $StringAddition = function($key, $value)
-        {
-            global $columns; 
-            global $values; 
-            global $date_collumns; 
-            global $year_number; 
-            $key = str_replace(' ','_',$key);
-            $key = str_replace("/","_",$key); 
-            $key = str_replace("-","",$key); 
-            $value = str_replace("'","\'",$value); 
-
-            $columns.=$key.',';
-            $value = (in_array(strtolower($key), $date_collumns))?"STR_TO_DATE('".substr_replace($value,"/".$year_number,strrpos($value,"/"),1)."','%m/%d/%Y')":"'".$value."'"; 
-            $values.= $value.",";  
-        }; 
-
-        foreach ($row as $key => $value) 
-        {
-            if(!in_array($key,$exclude_columns))
-            {
-                if(isset($get_id[$key]))
-                {
-                    $StringAddition($get_id[$key]['change'], Connect::GetId($get_id[$key]['search'],$value,$get_id[$key]['table'])); 
-                }
-                else 
-                {
-                    $StringAddition($key, $value); 
-                }
-
-
-            }
-        }       
-        
-        $columns = substr_replace($columns, "",strrpos($columns,","),1) . ")"; 
-        $values = substr_replace($values, "",strrpos($values,","),1) . ")";
-        $sql.= $columns."\n".$values.";"; 
-        array_push($queries,$sql); 
-    }
-
-    $result = Connect::ExecTransaction($queries); 
-    echo $result; 
+    ExecExcelCommand($table, $date_collumns, $exclude_columns, $get_id, $year_number, $comma); 
 ?>
