e066cba80824b98b2fbf6a8098c54c358ea0a635
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-15 00:18:59 +1200

    modify and get scrolling table and things done

--- js/setup.js
+++ js/setup.js
@@ -8,20 +8,66 @@ jQuery
                 state: 
                 {
                     controller: "", 
-                    action: "overview"
+                    action: "Overview"
                 }, 
                 mutations: 
                 {
-                    ChangeController(state,payload)
+                    RedirectUrl
+                    (
+                        state, 
+                        {
+                            param="", 
+                            value=""
+                        }
+                    )
                     {
-                        state.controller = payload; 
-                        state.action = "overview"; 
+                        if(param=="")
+                        {
+                            window.history.pushState("","",window.location.pathname); 
+                            state.controller = ""; 
+                            state.action = ""; 
+                            return; 
+                        }
+                        var search_params = (param=="controller")?"": window.location.search; 
+                        var url_params = new URLSearchParams(search_params); 
+                        url_params.set(param,value); 
+                        if(param=="controller")
+                        {
+                            state.controller = url_params.get(param); 
+                        }
+                        window.history.pushState(value,value,"?"+url_params.toString()); 
                     }
                 }
             }
 
         );
 
+        function GetController()
+        {
+            var search_params = window.location.search; 
+            if(search_params=="")
+            {
+                window.store_track.commit('RedirectUrl',{});
+                return; 
+            }
+            var url_params = new URLSearchParams(search_params); 
+            url_params.forEach
+            (
+                (value, param)=>
+                {
+                    window.store_track.commit
+                    (
+                        'RedirectUrl',
+                        {
+                            param: param, 
+                            value: value
+                        }
+                    );
+                }
+            ); 
+        }
+
+        GetController(); 
         CreateNavbar();
         CreateWrapper(); 
     }
---
+++ js/wrapper/scrolling_table.js
@@ -0,0 +1,93 @@
+var scrolling_table = Vue.component
+(
+    "scrolling-table", 
+    {
+        data() 
+        {
+            return {
+                table_data: [], 
+                thead: {}, 
+                tbody: []
+            };
+        }, 
+
+        mounted() 
+        {
+            if(window.store_track.state.action!="Overview")
+            {
+                return; 
+            }
+
+            var url = "server/overview_controller/"+window.store_track.state.controller+".php"; 
+            var data = AjaxRequest(url);
+            try 
+            {
+                this.table_data = JSON.parse(data); 
+            }
+            catch 
+            {
+                return; 
+            }          
+        },
+
+        watch: 
+        {
+            table_data: function()
+            {
+                if(this.table_data!=[])
+                {
+                    let thead = {}; 
+                    let index = 0; 
+                    this.table_data.forEach
+                    (
+                        row => 
+                        {
+                            for(var key of Object.keys(row))
+                            {
+                                if(thead[key]==undefined)
+                                {
+                                    thead[key] = index++; 
+                                }
+                            }
+                        }
+                    );
+                    this.thead = thead; 
+                }
+            }, 
+            thead: function()
+            {
+                this.tbody = []; 
+                this.table_data.forEach
+                (
+                    row => 
+                    {
+                        let data_row = Array(Object.keys(this.thead).length).fill(" "); 
+                        for(var key of Object.keys(row))
+                        {
+                            data_row[this.thead[key]] = row[key]; 
+                        }
+                        this.tbody.push(data_row); 
+                    }
+                );
+            }
+        }, 
+
+        template: 
+        `
+            <div class="scrolling-div">
+                <table v-if="table_data!=[]" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th v-for="key in Object.keys(thead)">{{key}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="row in tbody">
+                            <td v-for="data in row">{{data}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        ` 
+    }
+); 
