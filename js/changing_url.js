3bf621e1c90fe50ea4f72485e57a7cc25b12cbb1
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-14 19:46:48 +1200

    get the vue working for the data in the page
    Former-commit-id: 8cb53aaaa0bdc78b70c880002e816850ecf72e79 [formerly 835c327022df0d423230748a9ebe84c4a86a9a78]
    Former-commit-id: 6627144997cc6a94435bc02c441771d7665c0e88

--- js/changing_url.js
+++ js/changing_url.js
@@ -1,8 +1,18 @@
-function RedirectUrl(param, value)
+function RedirectUrl(param="", value="")
 {
-    var search_params = (param=="controller")?"":window.location.search; 
+    if(param=="")
+    {
+        window.history.pushState("","",window.location.pathname); 
+        window.store_track.commit("ChangeController", ""); 
+        return; 
+    }
+    var search_params = (param=="controller")?"": window.location.search; 
     var url_params = new URLSearchParams(search_params); 
     url_params.set(param,value); 
+    if(param=="controller")
+    {
+        window.store_track.commit("ChangeController", url_params.get(param)); 
+    }
     window.history.pushState(value,value,"?"+url_params.toString()); 
 }
 
