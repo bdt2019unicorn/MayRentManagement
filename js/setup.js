74563091d7cceb24da8862ab3d56df2933779ad2
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-15 14:30:17 +1200

    get the working order right and tidy up component
    Former-commit-id: 64590b2f77897343a2afdb8e1859f5125f888c11

--- js/setup.js
+++ js/setup.js
@@ -2,73 +2,90 @@ jQuery
 (
     function()
     {
-        window.store_track = new Vuex.Store
-        (
-            {
-                state: 
-                {
-                    controller: "", 
-                    action: "Overview"
-                }, 
-                mutations: 
-                {
-                    RedirectUrl
-                    (
-                        state, 
-                        {
-                            param="", 
-                            value=""
-                        }
-                    )
-                    {
-                        if(param=="")
-                        {
-                            window.history.pushState("","",window.location.pathname); 
-                            state.controller = ""; 
-                            state.action = ""; 
-                            return; 
-                        }
-                        var search_params = (param=="controller")?"": window.location.search; 
-                        var url_params = new URLSearchParams(search_params); 
-                        url_params.set(param,value); 
-                        if(param=="controller")
-                        {
-                            state.controller = url_params.get(param); 
-                        }
-                        window.history.pushState(value,value,"?"+url_params.toString()); 
-                    }
-                }
-            }
-
-        );
-
-        function GetController()
+        function CreateStoreTrack()
         {
-            var search_params = window.location.search; 
-            if(search_params=="")
-            {
-                window.store_track.commit('RedirectUrl',{});
-                return; 
-            }
-            var url_params = new URLSearchParams(search_params); 
-            url_params.forEach
+            return new Promise 
             (
-                (value, param)=>
+                (resolve,reject)=>
                 {
-                    window.store_track.commit
+                    var store_track = new Vuex.Store
                     (
-                        'RedirectUrl',
                         {
-                            param: param, 
-                            value: value
+                            state: 
+                            {
+                                controller: "", 
+                                action: "Overview"
+                            }, 
+                            mutations: 
+                            {
+                                RedirectUrl
+                                (
+                                    state, 
+                                    {
+                                        param="", 
+                                        value=""
+                                    }
+                                )
+                                {
+                                    if(param=="")
+                                    {
+                                        window.history.pushState("","",window.location.pathname); 
+                                        state.controller = ""; 
+                                        state.action = ""; 
+                                        return; 
+                                    }
+                                    var search_params = (param=="controller")?"": window.location.search; 
+                                    var url_params = new URLSearchParams(search_params); 
+                                    url_params.set(param,value); 
+                                    if(param=="controller")
+                                    {
+                                        state.controller = url_params.get(param); 
+                                    }
+                                    window.history.pushState(value,value,"?"+url_params.toString()); 
+                                }
+                            }
                         }
+            
                     );
+                    window.store_track = store_track; 
+                    resolve(store_track); 
                 }
-            ); 
+            )
         }
 
-        GetController(); 
-        CreateNavbar();
-        CreateWrapper(); 
+        function GetController(store_track)
+        {
+            return new Promise 
+            (
+                (resolve,reject)=>
+                {
+                    var search_params = window.location.search; 
+                    if(search_params=="")
+                    {
+                        store_track.commit('RedirectUrl',{});
+                        resolve(); 
+                    }
+                    var url_params = new URLSearchParams(search_params); 
+                    url_params.forEach
+                    (
+                        (value, param)=>
+                        {
+                            store_track.commit
+                            (
+                                'RedirectUrl',
+                                {
+                                    param: param, 
+                                    value: value
+                                }
+                            );
+                        }
+                    ); 
+                    resolve(); 
+                }
+            )
+
+        }
+
+        CreateStoreTrack().then(GetController).then(CreateWrapper).then(CreateWrapper).then(CreateNavbar); 
     }
 ); 
