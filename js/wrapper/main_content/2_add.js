e91830326e32782186f42f2e218cdcb74deef246
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-16 10:52:27 +1200

    get the add component on the game

--- js/wrapper/main_content/2_add.js
+++ js/wrapper/main_content/2_add.js
@@ -54,30 +54,30 @@ var row_group = Vue.component
 (
     "row-group", 
     {
-        // props: ["row"], 
-        data() 
-        {
-            return {
-                row: 
-                [
-                    {
-                        component: "text-input", 
-                        name: "first_name", 
-                        title: "First Name"
-                    }, 
-                    {
-                        component: "text-input", 
-                        name: "last_name", 
-                        title: "Last Name"
-                    }, 
-                    {
-                        component: "date-input", 
-                        name: "date_of_birth", 
-                        title: "Date of Birth"
-                    }
-                ]
-            }
-        }, 
+        props: ["row"], 
+        // data() 
+        // {
+        //     return {
+        //         row: 
+        //         [
+        //             {
+        //                 component: "text-input", 
+        //                 name: "first_name", 
+        //                 title: "First Name"
+        //             }, 
+        //             {
+        //                 component: "text-input", 
+        //                 name: "last_name", 
+        //                 title: "Last Name"
+        //             }, 
+        //             {
+        //                 component: "date-input", 
+        //                 name: "date_of_birth", 
+        //                 title: "Date of Birth"
+        //             }
+        //         ]
+        //     }
+        // }, 
         template: 
         `
             <div>
@@ -103,6 +103,46 @@ var add_component = Vue.component
 (
     "add-component", 
     {
+        data()
+        {
+            return {
+                form: [], 
+                title: "Add"
+            }
+        }, 
+
+        created() 
+        {
+            var data = GiveMeFakeData(); 
+            this.form = data.form; 
+            this.title+=data.title; 
+        },
+
+        template: 
+        `
+            <form class="container-fluid">
+
+                <row-group
+                    v-for="row in form"
+                    :row="row"
+                >
+                </row-group>
+            
+                <div>
+                    <br>
+                    <div class="row">
+                        <div class="form-group col-10">
+                        </div>
+                        <div class="form-group col-2">
+                            <button type="submit" class="btn">
+                                <i style="font-size: xx-large;" class="fas fa-arrow-alt-circle-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        `
     }
 ); 
 
@@ -115,5 +155,77 @@ var add_component = Vue.component
 
 function GiveMeFakeData()
 {
-
+    var form =  
+    [
+        [
+            {
+                component: "text-input", 
+                name: "first_name", 
+                title: "First Name"
+            }, 
+            {
+                component: "text-input", 
+                name: "last_name", 
+                title: "Last Name"
+            }, 
+            {
+                component: "date-input", 
+                name: "date_of_birth", 
+                title: "Date of Birth"
+            }
+        ], 
+        [
+            {
+                component: "text-input", 
+                name: "passport_id_number", 
+                title: "Passport/ID Number"
+            }, 
+            {
+                component: "text-input", 
+                name: "Nationality", 
+                title: "Nationality"
+            }
+        ], 
+        [
+            {
+                component: "text-input", 
+                name: "mobile_phone", 
+                title: "Mobile Phone"
+            }, 
+            {
+                component: "text-input", 
+                name: "work_phone", 
+                title: "Work Phone"
+            }
+        ], 
+        [
+            {
+                component: "text-input", 
+                name: "personal_email", 
+                title: "Personal Email"
+            }, 
+            {
+                component: "text-input", 
+                name: "work_email", 
+                title: "Work Email"
+            }
+        ], 
+        [
+            {
+                component: "text-input", 
+                name: "company_name", 
+                title: "Company Name"
+            }, 
+            {
+                component: "text-input", 
+                name: "company_address", 
+                title: "Company Address"
+            }
+        ]
+    ]
+    var title = "Tenant"; 
+    return {
+        title: title, 
+        form: form
+    }; 
 }
