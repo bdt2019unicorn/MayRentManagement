9f4cfe63c3f8279a7178d74a08e470fcbee1fc94
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-16 23:20:00 +1200

    final event things - just need to get the submit works and then that is it
    Former-commit-id: 40a62dd1a8d083efd6eb4cc677fc38ffcc772152

--- js/wrapper/main_content/1_overview.js
+++ js/wrapper/main_content/1_overview.js
@@ -2,12 +2,31 @@ var overview_component = Vue.component
 (
     "Overview", 
     {
+        computed: 
+        {
+            TableData()
+            {
+                var url = "server/overview_controller/"+window.store_track.state.controller+".php";
+                var data = AjaxRequest(url);
+    
+                try 
+                {
+                   return JSON.parse(data); 
+                }
+                catch
+                {
+                    return []; 
+                }          
+            }   
+        },
+
         template: 
         `
             <div class="row">
                 <div class="col-1"></div>
                 <scrolling-table
                     extra_class="col-10"
+                    :table_data="TableData"
                 >
                 </scrolling-table>
                 <div class="col-1"></div>
