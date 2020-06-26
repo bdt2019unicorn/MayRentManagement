8660034d5ccb12a76156da7fa334c330975f4d61
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-27 00:18:30 +1200

    modify the page to get for the login and other controller thing finished
    Former-commit-id: 093205b8907a3ab2acf2ef2bce86918165169fc9 [formerly 20cf8ebf3f391a2cf6bf259fceaaf082705cdbc9]
    Former-commit-id: c319c0c892b1e602fff420d53e9e471fdb7acded

--- js/full_page/page-wrapper/main_content/1_overview.js
+++ js/full_page/page-wrapper/main_content/1_overview.js
@@ -2,21 +2,14 @@ var overview_component = Vue.component
 (
     "Overview", 
     {
-        mixins:[overview_data_mixins], 
-        computed: 
-        {
-            OverviewController()
-            {
-                return window.store_track.state.controller; 
-            }   
-        },
+        mixins:[support_mixin], 
         template: 
         `
             <div class="row">
                 <div class="col-1"></div>
                 <scrolling-table
                     extra_class="col-10"
-                    :table_data="TableData(OverviewController)"
+                    :table_data="TableData(Controller)"
                 >
                 </scrolling-table>
                 <div class="col-1"></div>
