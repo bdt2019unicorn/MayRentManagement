e586a2476603600ea41e16090b2bcabe7725dc05
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-06-30 16:50:27 +1200

    Update scrolling_table.js
    Former-commit-id: de6a084f077b2064f271a1e66026180c134776ad

--- js/full_page/page-wrapper/main_content/scrolling_table.js
+++ js/full_page/page-wrapper/main_content/scrolling_table.js
@@ -1,3 +1,53 @@
+var th_sort_table = Vue.component
+(
+    "a-sort-table", 
+    {
+        props: ["text", "index"], 
+        data()
+        {
+            return {
+                current_operation: "<", 
+                operation_switch: 
+                {
+                    ">": "<", 
+                    "<": ">"
+                }
+            }
+        }, 
+        methods: 
+        {
+            SortTable()
+            {
+                let script = 
+                `
+                    while (Number(items[i][key]) ${this.current_operation} pivot) 
+                    {
+                        i++;
+                    }
+                    while (Number(items[j][key]) ${this.operation_switch[this.current_operation]} pivot) 
+                    {
+                        j--;
+                    }
+                `; 
+                this.$emit("sort-table", this.index, script); 
+                this.current_operation = this.operation_switch[this.current_operation]; 
+            }    
+        },
+        template: 
+        `
+            <a 
+                href="javascript:void(0);" 
+                class="text-white" 
+                @click="SortTable"
+            >
+                {{text}}
+            </a>
+        `
+    }
+)
+
+
+
 var scrolling_table = Vue.component
 (
     "scrolling-table", 
@@ -12,11 +62,11 @@ var scrolling_table = Vue.component
         }, 
         computed: 
         {
-            HiddenColums()
+            HiddenColums() //get rid of later 
             {
                 return this.SpecialColumnsIndexes("hidden_columns"); 
             },     
-            SortColumns()
+            SortColumns() // get rid of later 
             {
                 return this.SpecialColumnsIndexes("sort"); 
             }, 
@@ -34,54 +84,44 @@ var scrolling_table = Vue.component
         {
             SetupTable()
             {
-                function THead(table_data)
+                THead = ()=>
                 {
-                    if(table_data!=[])
+                    this.thead = {}; 
+                    if(this.table_data[0])
                     {
-                        let thead = {}; 
                         let index = 0; 
-                        table_data.forEach
+                        for(var key of Object.keys(this.table_data[0]))
+                        {
+                            if(!this.thead[key])
+                            {
+                                this.thead[key] = index++; 
+                            }
+                        }
+                    }
+                }
+
+                TBody = ()=>
+                {
+                    this.tbody = []; 
+                    if(this.thead!={})
+                    {
+                        this.table_data.forEach
                         (
                             row => 
                             {
+                                let data_row = Array(Object.keys(this.thead).length).fill(" "); 
                                 for(var key of Object.keys(row))
                                 {
-                                    if(thead[key]==undefined)
-                                    {
-                                        thead[key] = index++; 
-                                    }
+                                    data_row[this.thead[key]] = row[key]; 
                                 }
+                                this.tbody.push(data_row); 
                             }
                         );
-                        return thead; 
                     }
-                    return {}; 
                 }
 
-                function TBody(thead, table_data)
-                {
-                    if(thead=={})
-                    {
-                        return []; 
-                    }
-                    var tbody = []; 
-                    table_data.forEach
-                    (
-                        row => 
-                        {
-                            let data_row = Array(Object.keys(thead).length).fill(" "); 
-                            for(var key of Object.keys(row))
-                            {
-                                data_row[thead[key]] = row[key]; 
-                            }
-                            tbody.push(data_row); 
-                        }
-                    );
-                    return tbody; 
-                }
-
-                this.thead = THead(this.table_data); 
-                this.tbody = TBody(this.thead, this.table_data); 
+                THead(); 
+                TBody(); 
             }, 
             SpecialColumnsIndexes(action)
             {
@@ -103,9 +143,50 @@ var scrolling_table = Vue.component
                     return {}; 
                 }
             }, 
-            SortTable(key)
+            SortTable(key, script)
             {
-                console.log(key); 
+                function Swap(items, leftIndex, rightIndex)
+                {
+                    var temp = items[leftIndex];
+                    items[leftIndex] = items[rightIndex];
+                    items[rightIndex] = temp;
+                }
+                Partition = (items, left, right) =>
+                {
+                    var pivot   = Number(items[Math.floor((right + left) / 2)][key]), 
+                        i       = left, 
+                        j       = right;
+                    while (i <= j) 
+                    {
+                        eval(script); 
+                        if (i <= j) 
+                        {
+                            Swap(items, i, j); 
+                            i++;
+                            j--;
+                        }
+                    }
+                    return i;
+                }
+                
+                function QuickSort(items, left, right) 
+                {
+                    var index;
+                    if (items.length > 1) 
+                    {
+                        index = Partition(items, left, right); 
+                        if (left < index - 1) 
+                        { 
+                            QuickSort(items, left, index - 1);
+                        }
+                        if (index < right) 
+                        { 
+                            QuickSort(items, index, right);
+                        }
+                    }
+                    return items;
+                }
+                this.tbody = QuickSort(this.tbody, 0, this.tbody.length-1).reverse(); 
             }
         },
 
@@ -129,16 +210,16 @@ var scrolling_table = Vue.component
                             <th 
                                 v-for="key in Object.keys(thead)" 
                                 :key="key"
-                                v-if="!HiddenColums[key]"
+                                v-if='!SpecialColumnsIndexes("hidden_columns")[key]'
                                 class="sticky-top"
                             >
-                                <a 
-                                    href="javascript:void(0);" 
-                                    class="text-white" 
-                                    v-if="SortColumns[key]" 
-                                    @click="SortTable(SortColumns[key])">
-                                    {{key}}
-                                </a>
+                                <a-sort-table
+                                    v-if='SpecialColumnsIndexes("sort")[key]' 
+                                    :text="key"
+                                    :index='SpecialColumnsIndexes("sort")[key]'
+                                    @sort-table="SortTable"
+                                >
+                                </a-sort-table>
                                 <template v-else>
                                     {{key}}
                                 </template>
@@ -150,7 +231,7 @@ var scrolling_table = Vue.component
                             <td 
                                 v-for="index in row.length"
                                 :key="index-1"
-                                v-if="!Object.values(HiddenColums).includes(index-1)"
+                                v-if='!Object.values(SpecialColumnsIndexes("hidden_columns")).includes(index-1)'
                             >
                                 {{row[index-1]}}
                             </td>
