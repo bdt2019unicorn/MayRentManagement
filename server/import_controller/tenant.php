482affbc6a3b6a67e6b6a729a0109b991a5106f5
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-03 12:33:51 +1200

    change the php for consistency and put the apartment excel template
    Former-commit-id: 40bd0af2a770c1a8bd0dc3701e0b915a097f0c41

--- server/import_controller/tenant.php
+++ server/import_controller/tenant.php
@@ -2,7 +2,7 @@
     require_once("main_controller.php"); 
     $table = "tenant"; 
     $date_collumns = ["date_of_birth"]; 
-    $exclude_columns = ["Tenant_ID", "Apartment"]; 
+    $exclude_columns = ["tenant_id", "apartment"]; 
     $get_id = []; 
     $year_number = "19"; 
     $comma = []; 
