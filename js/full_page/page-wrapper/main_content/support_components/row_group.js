cfa16fff765897b355f4179d4503218b703bded0
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-04 13:37:56 +1200

    modify the date required

--- js/full_page/page-wrapper/main_content/support_components/row_group.js
+++ js/full_page/page-wrapper/main_content/support_components/row_group.js
@@ -1,8 +1,46 @@
-var row_group = Vue.component
+Vue.component
 (
     "row-group", 
     {
-        props: ["row","just_started_parent", "controller"], 
+        props: ["row","just_started_parent", "controller", "index"],
+        data() 
+        {
+            return {
+                date_required: {}, 
+                date_group: {}, 
+                row_group_valid: true 
+            }
+        }, 
+        mounted() 
+        {
+            this.row_group_valid = this.RowGroupValid(); 
+            this.$emit("row-group-mounted", this.index, this.row_group_valid); 
+        },
+        methods: 
+        {
+            RowGroupValid()
+            {
+                return (Object.values(this.date_required).length>0)?!(Object.values(this.date_required).includes(false)): true; 
+            }, 
+            DateInputValidation(data_field, name, boolean)
+            {
+                this[data_field][name] = boolean; 
+                this.row_group_valid = this.RowGroupValid(); 
+            }
+        },
+        watch: 
+        {
+            row: function(new_value, old_value)
+            {
+                this.date_required = {}; 
+                this.date_group = {}; 
+                this.row_group_valid = true 
+            }, 
+            row_group_valid: function(new_value, old_value)
+            {
+                this.$emit("row-group-validation", this.index, this.row_group_valid); 
+            }
+        },
         template: 
         `
             <div class="row">
@@ -12,6 +50,7 @@ var row_group = Vue.component
                     v-bind="col"
                     :just_started_parent="just_started_parent"
                     :controller="controller"
+                    @date-input-validation="DateInputValidation"
                 >
                 </component>
 
