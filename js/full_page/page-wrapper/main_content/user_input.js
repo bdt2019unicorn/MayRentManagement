ccb848a8cb02e42bd111914e5e4ba15afb44fc9a
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-05 14:15:01 +1200

    make the test for user input works
    The user input is split into 2 function add and edit
    The user edit works
    The Add function is working

--- js/full_page/page-wrapper/main_content/user_input.js
+++ js/full_page/page-wrapper/main_content/user_input.js
@@ -1,6 +1,6 @@
 Vue.component
 (
-    "user-input-test", 
+    "user-input", 
     {
         props: ["title","form", "validate"], 
         mixins: [support_mixin], 
@@ -38,23 +38,7 @@ Vue.component
                     return; 
                 }
                 let data = $(event.target).serializeObject(); 
-                console.log(data); 
-                // let result = this.ImportData([data], this.controller); 
-                // if(result==true)
-                // {
-                //     alert(this.title+" Success!"); 
-                //     $(this.$refs["action_form"]).trigger("reset"); 
-                //     if(this.controller)
-                //     {
-                //         this.$emit("valid-controller-success", this.controller, data); 
-                //         return; 
-                //     }
-                //     window.location.reload(); 
-                // }
-                // else
-                // {
-                //     alert(this.title+" Fails, please try again!"); 
-                // }
+                this.$emit("form-information-valid", data); 
             }
         },
 
@@ -85,161 +69,6 @@ Vue.component
                         :row="row"
                         :just_started_parent= "just_started_parent"
                         :controller="StateController"
-                        :index="index"
-                        :key="index"
-                        @row-group-validation="RowGroupValidation"
-                    >
-                    </row-group>
-                </template>
-            
-                <br>
-                <div class="row">
-                    <div class="form-group col-2">
-                        <button type="reset" class="btn" title="Clear">
-                            <i style="font-size: xx-large;" class="fas fa-times"></i>
-                        </button>
-                    </div>
-                    <div class="form-group col-8">
-                    </div>
-                    <div class="form-group col-2">
-                        <button type="submit" class="btn" :title="title">
-                            <i style="font-size: xx-large;" class="fas fa-arrow-alt-circle-right"></i>
-                        </button>
-                    </div>
-                </div>
-
-            </form>
-        `
-    }
-); 
-
-
-
-
-
-
-
-
-
-
-Vue.component
-(
-    "user-input", 
-    {
-        props: ["controller"], 
-        mixins: [support_mixin], 
-        data()
-        {
-            return {
-                title: "Add ",
-                just_started_parent: false,  
-                form: [], 
-                validate: {}, 
-                row_valid: {}
-            }
-        }, 
-        computed: 
-        {
-            CurrentController()
-            {
-                return ((this.controller)?this.controller:this.StateController); 
-            }
-        },
-        methods: 
-        {        
-            PopulateFormData()
-            {
-                var data = this.AjaxRequest(`server/user_input_controller/${this.CurrentController}.json`); 
-                try 
-                {
-                    this.form = data.form; 
-                    this.title = (this.controller)?data.title: this.title+data.title; 
-                    this.validate = (data.validate)?data.validate:this.validate; 
-                } 
-                catch
-                {
-                    this.form = []; 
-                    this.title ="Add "; 
-                }
-            }, 
-            PopulateFormValidation()
-            {
-                $(this.$refs["action_form"]).trigger("reset"); 
-                $(this.$refs["action_form"]).validate().destroy(); 
-                $(this.$refs["action_form"]).validate(this.validate); 
-            }, 
-            FormValid()
-            {
-                return (
-                    $(this.$refs["action_form"]).valid() && 
-                    this.ValidObject(this.row_valid)
-                ); 
-            }, 
-            RowGroupValidation(index, validation)
-            {
-                this.row_valid[index] = validation; 
-            }, 
-            SubmitForm(event)
-            {
-                this.just_started_parent = true; 
-                if(!this.FormValid())
-                {
-                    return; 
-                }
-                let data = $(event.target).serializeObject(); 
-                let result = this.ImportData([data], this.controller); 
-                if(Number(result))
-                {
-                    alert(this.title+" Success!"); 
-                    $(this.$refs["action_form"]).trigger("reset"); 
-                    if(this.controller)
-                    {
-                        data["user_id"] = Number(result); 
-                        this.$emit("valid-controller-success", this.controller, data); 
-                        return; 
-                    }
-                    window.location.reload(); 
-                }
-                else
-                {
-                    alert(this.title+" Fails, please try again!"); 
-                }
-            }
-        },
-
-        watch: 
-        {
-            CurrentController: function(new_value, old_value)
-            {
-                this.PopulateFormData(); 
-                this.PopulateFormValidation(); 
-            }   
-        },
-
-        mounted()
-        {
-            this.PopulateFormValidation(); 
-        }, 
-
-        created() 
-        {
-            this.PopulateFormData(); 
-        },
-
-        template: 
-        `
-            <form 
-                class="container-fluid"
-                @submit.prevent="SubmitForm"
-                ref="action_form"
-            >
-                <h1 style="text-align: center;">{{title}}</h1>
-                <template v-for="(row, index) in form">
-                    <br>
-                    <row-group
-                        :row="row"
-                        :just_started_parent= "just_started_parent"
-                        :controller="CurrentController"
                         :index="index"
                         :key="index"
                         @row-group-validation="RowGroupValidation"
---
+++ server/helper/query.php
@@ -0,0 +1,48 @@
+<?php 
+    class Query
+    {
+    	static public function Insert($table, $data)
+    	{
+    		$sql = "INSERT INTO `{$table}`"; 
+    		$columns = "("; 
+    		$values = "VALUES ("; 
+
+    		foreach ($data as $column => $value) 
+    		{
+    			$columns.="`{$column}`,"; 
+    			$values.="'{$value}',"; 
+    		}
+    		$sql.=" ".Query::RemoveLastCharacter($columns, ',').") ".Query::RemoveLastCharacter($values, ',').");"; 
+    		return $sql; 
+    	}
+
+        static public function Update($table, $data, $conditions)
+        {
+        	$sql = "UPDATE `{$table}` SET "; 
+
+        	foreach ($data as $column => $value) 
+        	{
+        		$sql.="`{$column}` = '{$value}', "; 
+        	}
+
+        	$sql = Query::RemoveLastCharacter($sql, ","); 
+        	$sql.= " ". Query::Where($conditions); 
+        	return $sql; 
+        }
+
+        static private function Where($conditions)
+        {
+            $sql = "WHERE "; 
+            foreach ($conditions as $key => $value) 
+            {
+            	$sql.="`{$key}` = '{$value}' AND "; 
+            }
+            return Query::RemoveLastCharacter($sql, "AND"); 
+        }
+
+        static private function RemoveLastCharacter($sql, $character)
+        {
+        	return substr($sql,0, strrpos($sql, $character)); 
+        }
+    }
+?>
