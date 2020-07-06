5d767e405349389cb0fdc3ce659c895aa5c0079b
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-07 02:18:06 +1200

    fixing the issue with the number input not getting the data
    Former-commit-id: 16596005604244e22b4f73400fff733a87b7487c

--- server/overview_controller/leaseagrm.php
+++ server/overview_controller/leaseagrm.php
@@ -1,18 +1,26 @@
 <?php 
 
 	require_once("./general.php"); 
-	$sql = 
-	"
+	if(isset($_GET['building_id']))
+	{
+		$sql = 
+		"
+	
+			SELECT `leaseagrm`.`id` as `ID`, `leaseagrm`.`name` AS `Name`, apartment.name as `Apartment`, CONCAT(tenant.Last_Name,', ',tenant.First_Name) AS `Tenant Name`, `Start_date`, `Finish`, `Rent_amount`, `Deposit_amount`, `Deposit_payment_date`, `Deposit_payback_date`, `Monthly_payment_date`, `Deposit_currency`, `Deposit_exchange_rate` 
+	
+			FROM `leaseagrm`, `apartment`, `tenant`
+			WHERE 
+				`leaseagrm`.`apartment_id` = `apartment`.`id` AND 
+				`leaseagrm`.`Tenant_ID` = `tenant`.`id` AND 
+				`apartment`.`building_id` = '{$_GET['building_id']}'; 
+		"; 
+	
+		GetDataGeneral($sql); 
+	}
 
-		SELECT `leaseagrm`.`id` as `ID`, `leaseagrm`.`name` AS `Name`, apartment.name as `Apartment`, CONCAT(tenant.Last_Name,', ',tenant.First_Name) AS `Tenant Name`, `Start_date`, `Finish`, `Rent_amount`, `Deposit_amount`, `Deposit_payment_date`, `Deposit_payback_date`, `Monthly_payment_date`, `Deposit_currency`, `Deposit_exchange_rate` 
-
-		FROM `leaseagrm`, `apartment`, `tenant`
-		WHERE 
-			`leaseagrm`.`apartment_id` = `apartment`.`id` AND 
-		    `leaseagrm`.`Tenant_ID` = `tenant`.`id` AND 
-		    `apartment`.`building_id` = '{$_GET['building_id']}'; 
-	"; 
-
-	GetDataGeneral($sql); 
+	else 
+	{
+		GetDataFromTable("leaseagrm", $_GET); 
+	}
 
 ?>
