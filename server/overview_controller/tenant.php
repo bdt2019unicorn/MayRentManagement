a651035da50f571c543f2eb9bbf5826a48366390
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-07 02:18:06 +1200

    fixing the issue with the number input not getting the data
    Former-commit-id: 60e304e295b6b7a1005817d1350dd6f0dd696f63 [formerly 16596005604244e22b4f73400fff733a87b7487c]
    Former-commit-id: 5d767e405349389cb0fdc3ce659c895aa5c0079b

--- server/overview_controller/tenant.php
+++ server/overview_controller/tenant.php
@@ -4,7 +4,12 @@
 	$sql = 
 	"
 		SELECT *, concat(`First_Name`, ' ', `Middle_Name`,' ', `Last_Name`) as `full_name` 
-		FROM `tenant`;
+		FROM `tenant`
 	"; 
+
+	if(isset($_GET["id"]))
+	{
+		$sql.=" WHERE id = '{$_GET['id']}' "; 
+	}
 	GetDataGeneral($sql); 
 ?>
