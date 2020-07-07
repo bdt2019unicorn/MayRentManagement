d9d2ca2567ed9abe4e5430aea724b8d961d477b1
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-07 16:38:50 +1200

    innitialize the hyperlink in the table
    Former-commit-id: e7c8192cc29b31dd88fa9959ecf62d7d62f503e8

---
+++ js/full_page/page-wrapper/main_content/support_components/scrolling_table/a_hyperlink.js
@@ -0,0 +1,25 @@
+Vue.component
+(
+    "a-hyperlink", 
+    {
+        props: ["controller", "object_id", "text"], 
+        methods: 
+        {
+            EditObject()
+            {
+                window.store_track.commit("RedirectUrl", {param: "controller", value: this.controller});
+                window.store_track.commit("RedirectUrl", {param: "action", value: "edit"}); 
+                window.store_track.commit("RedirectUrl", {param: "object_id", value: this.object_id}); 
+            }
+        },
+        template: 
+        `
+            <a 
+                href="javascript:void(0);" 
+                @click="EditObject"
+            >
+                {{text}}
+            </a>
+        `
+    }
+); 
