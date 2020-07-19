db2ef4b0bdb5715804707f230388cb8458df41e1
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-19 14:16:43 +1200

    get data utility

--- js/full_page/page-wrapper/main_content/utilities/utilities_overview.js
+++ js/full_page/page-wrapper/main_content/utilities/utilities_overview.js
@@ -7,70 +7,106 @@ Vue.component
         data()
         {
             return {
-                apartment_id: "", 
-                just_started_parent: true, 
+                date_picker: false, 
+                end_date: undefined, 
+                function_calendar_model: undefined, 
+                main_url: "server/utilities_controller/utility_overview.php?command=", 
                 select_data: 
-                [
-                    {
-                        id: 1, 
-                        value: "electric"
-                    }, 
-                    {
-                        id: 2, 
-                        value: "water"
-                    }
-                ], 
-                table_data: 
-                [
-                    {
-                        id: 1, 
-                        name: "test"
-                    }, 
-                    {
-                        id: 2, 
-                        name: "test"
-                    }
-                ], 
-                utility_id: "", 
-                test_picker: false, 
-                vmodel: undefined
+                {
+                    apartment: [], 
+                    utilities: [], 
+                    select_value: "id", 
+                    text: "name", 
+                    not_required: true
+                }, 
+                start_date: undefined, 
+                table_data: []
             }
         }, 
         components: {FunctionalCalendar}, 
-        mounted() 
+        created()
         {
-            ChooseDate = ()=>
-            {
-                return new Promise
-                (
-                    (resolve,reject)=>
-                    {
-                        this.$refs["Calendar"].ChooseDate("today"); 
-                        resolve(); 
-                    }
-                ); 
-            }; 
-            ChooseDate().then
-            (
-                ()=>
-                {
-                    this.vmodel.dateRange.start="2020-7-1"; 
-                    this.vmodel.dateRange.end="2020-7-21"; 
-                }
-            );
-        },
+            this.SelectData(); 
+            this.UtilitiesTableData(); 
+        }, 
         methods: 
         {
             TestBtn()
             {
-                
-                this.test_picker = !this.test_picker; 
+                this.date_picker = !this.date_picker; 
             }, 
             TestClickedRange()
             {
                 console.log("ranged"); 
-                console.log(this.vmodel); 
+                console.log(this.function_calendar_model); 
+            }, 
+
+            CalendarOpened()
+            {
+                new Promise
+                (
+                    (resolve,reject)=>
+                    {
+                        this.$refs["calendar"].ChooseDate("today"); 
+                        resolve(); 
+                    }
+                ).then 
+                (
+                    ()=>
+                    {
+                        return new Promise
+                        (
+                            (resolve, reject)=>
+                            {
+                                this.function_calendar_model.dateRange.start = this.start_date; 
+                                this.function_calendar_model.dateRange.end = this.end_date; 
+                                resolve(); 
+                            }
+                        ); 
+                    }
+                ).then 
+                (
+                    ()=>
+                    {
+                        this.function_calendar_model = undefined; 
+                    }
+                ); 
+                
+            }, 
+            SelectData()
+            {
+                let utility_data = this.AjaxRequest(`${this.main_url}SelectData`); 
+                this.select_data.utilities = JSON.parse(utility_data); 
+                this.select_data.apartment = this.TableData("apartment"); 
+            }, 
+            UtilitiesTableData()
+            {
+                this.table_data =                 
+                [
+                    {
+                        id: 1, 
+                        name: "test"
+                    }, 
+                    {
+                        id: 2, 
+                        name: "test"
+                    }
+                ]; 
             }
+        
+
+
+
+
+        },
+        mounted() 
+        {
+            var today = new Date(); 
+            let month = today.getMonth(); 
+            let year = today.getFullYear(); 
+            let last_day_of_month = new Date(year, month+1,0).getDate();   
+            this.start_date = `${year}-${month+1}-1`; 
+            this.end_date = `${year}-${month+1}-${last_day_of_month}`; 
         },
         template:
         `
@@ -80,30 +116,54 @@ Vue.component
                 <div class="row">
                     <form class="container-fluid col">  
                         <div class="row">
-                            <select-input :select_data="select_data" select_value="id" text="value" option_title="All Utilities" not_required="true"></select-input>
-                            <select-input :select_data="select_data" select_value="id" text="value" option_title="All Utilities" not_required="true"></select-input>
+                            <select-input :select_data="select_data.utilities" v-bind="select_data" name="utilities_id">All Utilities</select-input>
+                            <select-input :select_data="select_data.apartment" v-bind="select_data" name="apartment_id">All Apartment</select-input>
                             <div class="col-2">
                                 <button class="btn btn-primary">Search</button>
                             </div>
                         </div>
 
                         <div class="row">
-                            <input type="text" class="form-control" @click="TestBtn">
-                            <FunctionalCalendar 
-                                v-show='test_picker' 
-                                class="col"
-                                dateFormat="yyyy-mm-dd" 
-                                :is-date-range='true' 
-                                v-model="vmodel" 
-                                @dayClicked="TestClickedRange" 
-                                ref="Calendar"
-                            ></FunctionalCalendar>
+                            <div class="col">
+                                <input type="text" class="form-control" @click="TestBtn">
+                                <FunctionalCalendar 
+                                    ref="calendar" 
+                                    v-if='date_picker' 
+                                    class="col"
+                                    dateFormat="yyyy-mm-dd" 
+                                    :is-date-range='true' 
+                                    v-model="function_calendar_model" 
+                                    @dayClicked="TestClickedRange" 
+                                    @opened="CalendarOpened"
+                                ></FunctionalCalendar>
+
+                                <input type="text" name="start_date" v-model="start_date">
+                                <input type="text" name="end_date" v-model="end_date">
+                            </div>
+
+                            <div class="col-2">
+                                <button class="btn btn-primary">Add</button>
+                            </div>
                         </div>
                     </form>
                 </div>
+                <br>
                 <scrolling-table :table_data="table_data"></scrolling-table>
 
             </div>
         `
     }
 ); 
+
+
+
+
+
+// initialy, the function_calendar_model will be null 
+// if the thing is opened, if it is just started, then choose the date. Otherwise
+
+/// what we want, first start - choose the date for us. 
+// close, get the date then set it to undefined. 
+// mounted - choose the date then change the range 
+
+// opened - set the date - then set it to undefined. 
