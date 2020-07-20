12f7cd85962e3736b6def686263912ca5be00e84
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-20 18:22:00 +1200

    apartment utilitiest start working on
    Former-commit-id: 62e524fbfaaa43f2e490a8681a51760b18cc69d3 [formerly cd96ed45318cc1d3ebca6e24a8df00573d89f766]
    Former-commit-id: e8c35c9faef2abcf2a097793171bbb6e5d53e488

---
+++ js/full_page/page-wrapper/main_content/utilities/apartment_utilities.js
@@ -0,0 +1,12 @@
+Vue.component
+(
+    "apartment-utilities", 
+    {
+        template: 
+        `
+            <div>
+                <h1>Test apartment utilitiest </h1>
+            </div>
+        `
+    }
+); 
