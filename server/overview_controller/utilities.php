a51c170b4fc463f7ea04e02da053b6385810ffe4
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-23 01:38:29 +1200

    modify the overview for utilties to get the final values
    Former-commit-id: 4a498405309cc346247ef6d9e30261a6fe5d68ae

--- server/overview_controller/utilities.php
+++ server/overview_controller/utilities.php
@@ -2,36 +2,53 @@
     require_once("../helper/connect.php"); 
     require_once("../helper/query.php"); 
     
-    $where = 
+    $conditions_sql = 
     "
-        WHERE `apartment`.`building_id` = `buildings`.`id` 
-        AND `buildings`.`id` = '{$_GET['building_id']}'
-        AND `utility_reading`.`revenue_type_id` = '{$_POST['revenue_type_id']}' 
-        AND `utility_reading`.`date`>='{$_POST['start_date']}' 
-        AND `utility_reading`.`date`<='{$_POST['end_date']}'
+        FROM 
+            `apartment` LEFT JOIN `utility_reading` ON `apartment`.`id` = `utility_reading`.`apartment_id`,
+            `buildings`
+        WHERE 
+            `apartment`.`building_id` = `buildings`.`id` 
+            AND `buildings`.`id` = '{$_GET['building_id']}'
+            AND `utility_reading`.`revenue_type_id` = '{$_POST['revenue_type_id']}' 
+            AND `utility_reading`.`date`>='{$_POST['start_date']}' 
+            AND `utility_reading`.`date`<='{$_POST['end_date']}'
     "; 
-
     $apartments = null; 
 
     if(isset($_POST["apartment_id"]))
     {
         if($_POST["apartment_id"])
         {
-            $where.="AND `apartment`.`id`={$_POST['apartment_id']}"; 
+            $conditions_sql.="AND `apartment`.`id`='{$_POST['apartment_id']}'"; 
             $apartments = Connect::GetDataWithId($_POST["apartment_id"], "apartment"); 
         }
     }
-
-    $conditions_sql = 
-    "
-        FROM `apartment` LEFT JOIN `utility_reading` ON `apartment`.`id` = `utility_reading`.`apartment_id`, `buildings`
-        {$where}
-    "; 
+    $conditions_sql.="\nORDER BY `apartment`.`id`, `utility_reading`.`date`\n"; 
 	$full_data_sql = 
 	"
-        SELECT DISTINCT `apartment`.`id` AS `apartment_id`, `apartment`.`name` AS `Apartment Name`, `utility_reading`.`revenue_type_id`, `utility_reading`.`date`, `utility_reading`.`number`
+        SELECT DISTINCT 
+            `apartment`.`id` AS `apartment_id`, 
+            `apartment`.`name` AS `Apartment Name`, 
+            `utility_reading`.`revenue_type_id`, 
+            `utility_reading`.`date`, 
+            `utility_reading`.`number`, 
+            (
+                SELECT MAX(`ur`.`number`)
+                from `utility_reading` ur
+                where `ur`.`date` = 
+                                (
+                                    SELECT MAX(`u_r`.`date`)
+                                    FROM `utility_reading` u_r 
+                                    WHERE 
+                                        `utility_reading`.`date`>`u_r`.`date`
+                                        AND `u_r`.`apartment_id` = `utility_reading`.`apartment_id`
+                                        AND `u_r`.`revenue_type_id` = `utility_reading`.`revenue_type_id`
+                                )
+                    AND `ur`.`apartment_id` = `utility_reading`.`apartment_id`
+                    AND `ur`.`revenue_type_id` = `utility_reading`.`revenue_type_id`
+            ) AS 'smaller_number'
         {$conditions_sql}
-
     "; 
 
     $full_data = Connect::GetData($full_data_sql); 
@@ -57,7 +74,8 @@
         (
             "apartment_id" => $aparment["id"], 
             "Apartment Name" => $aparment["name"], 
-            "revenue_type_id" => $_POST["revenue_type_id"]
+            "revenue_type_id" => $_POST["revenue_type_id"], 
+            "apartment_table"=>array()
         ); 
 
         foreach ($dates as $date) 
@@ -74,6 +92,16 @@
         if($values["date"])
         {
             $data[$values["apartment_id"]][date_format(date_create($values["date"]), "d M Y")] = $values["number"]; 
+            
+            $consumption = doubleval($values["number"]) - doubleval($values["smaller_number"]); 
+            $data[$values["apartment_id"]]["apartment_table"][$values["date"]] =
+            array
+            (
+                "reading"=>$values["number"], 
+                "smaller_number"=>$values["smaller_number"], 
+                "consumption"=> $consumption
+            );  
+
         }
     }
 
