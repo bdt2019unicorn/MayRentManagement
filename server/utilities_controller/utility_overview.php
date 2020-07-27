05fdf21e68f4eb5621e9000cb05d88e9aec9c1db
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-27 23:58:17 +1200

    put the form for adding new current price
    Former-commit-id: c06ea6d976649b79687a1fa236ef442dd5aa4b3a

--- server/utilities_controller/utility_overview.php
+++ server/utilities_controller/utility_overview.php
@@ -7,6 +7,22 @@
             $sql = "SELECT * FROM `revenue_type` WHERE `is_utility`='1';"; 
             $select_data = Connect::GetData($sql); 
             echo(json_encode($select_data)); 
+        }, 
+        "CurrentPrice"=>function()
+        {
+            $sql = 
+            "
+                SELECT * FROM utility_price WHERE 
+                date_valid = 
+                (
+                    SELECT MAX(date_valid) 
+                    FROM utility_price up 
+                    WHERE up.revenue_type_id=utility_price.revenue_type_id
+                ) 
+                AND revenue_type_id = {$_GET['revenue_type_id']}
+            "; 
+            $current_price = Connect::GetData($sql); 
+            echo(json_encode($current_price)); 
         }
     ); 
 
