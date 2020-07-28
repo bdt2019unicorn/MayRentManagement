a53299db46bedd2885e102db805206bbed961b34
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-07-28 14:47:24 +1200

    Update utility_reading.js
    Former-commit-id: 56802266870f89b3b6e45afcbadb4cd7fa4b46f8

--- js/full_page/page-wrapper/utilities/utility_reading.js
+++ js/full_page/page-wrapper/utilities/utility_reading.js
@@ -2,10 +2,30 @@ Vue.component
 (
     "utility-reading", 
     {
+        mixins: [utilities_mixin], 
         template:
         `
-            <div>
-                <h1>Test</h1>
+            <div class="container-fluid">
+                <h1>Utilities</h1>
+                <h6>Add new utility reading</h6>
+
+                <div class="container-fluid">
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead class="thead-light">
+                            <tr class="text-center">
+                                <th>Service</th>
+                                <th>Apartment Name</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Reading</th>
+                                <th>Previous Reading</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                <div>
             </div>
         `
     }    
