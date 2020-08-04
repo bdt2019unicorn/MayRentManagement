3c1f9684d87bea7557ca3b06d0932973a3405573
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-08-05 11:48:47 +1200

    shorten the edit controller
    Former-commit-id: 30417fdc8f0f783ed00deac46306b0b9e8a22d3f

--- server/edit_controller.php
+++ server/edit_controller.php
@@ -1,10 +1,10 @@
 <?php 
-    require_once("../helper/database.php"); 
+    require_once("./helper/database.php"); 
 
     $post_key = "edit"; 
     $data = json_decode($_POST[$post_key]); 
 
-    $sql = Query::Update($table, $data, $_GET);
+    $sql = Query::Update($_GET["table"], $data, ["id"=>$_GET["id"]]);
     $result = Connect::GetData($sql); 
     echo $result; 
 ?>
