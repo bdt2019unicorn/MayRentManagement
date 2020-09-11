acb4dc9bbbde20e3c909194258fc4307dfa81b67
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-09-11 14:53:51 +1200

    Update add_invoice.js
    Former-commit-id: ad170fa6f2ac094168a7a6e28503104307d0dc76

--- js/full_page/page-wrapper/invoices/add_invoice.js
+++ js/full_page/page-wrapper/invoices/add_invoice.js
@@ -11,7 +11,7 @@ Vue.component
                 {
                     end_date: undefined, 
                     leaseagrm_id: undefined, 
-                    name: undefined, 
+                    name: "", 
                     start_date: undefined
                 }
 
@@ -22,13 +22,49 @@ Vue.component
         {
             LabelDateRange()
             {
-                return `${moment(this.invoice.start_date).format('DD MMM YYYY')} - ${moment(this.invoice.end_date).format('DD MMM YYYY')}`; 
+                return this.invoice.start_date?`${moment(this.invoice.start_date).format('DD MMM YYYY')} - ${moment(this.invoice.end_date).format('DD MMM YYYY')}`: ""; 
             }, 
         }, 
         methods: 
         {
             CalendarChooseDay()
             {
+                if(this.function_calendar_model.dateRange.end)
+                {
+                    this.invoice.start_date = this.function_calendar_model.dateRange.start; 
+                    this.invoice.end_date = this.function_calendar_model.dateRange.end; 
+                    this.function_calendar_model = undefined; 
+                    this.date_picker_opened = false; 
+                }
+            }, 
+            LeaseagrmIdSelectChanged()
+            {
+                this.leaseagrm_id = $(this.$refs["leaseagrm_id_select"]).find("[name='leaseagrm_id']").val(); 
+
+                console.log(this.leaseagrm_id); 
+
+// neeed to do some modification for this function so I can get the whole thing working. 
+// more work needed here. We need to add the function so that it can find the date and take the period possible. 
+
+
+                if(!this.invoice.name.trim().length>0)
+                {
+                    let test = $(this.$refs["leaseagrm_id_select"]).find("[name='leaseagrm_id']").children(); 
+                    console.log(test); 
+
+                    console.log(Object.keys(test).filter(key=>!isNaN(key))); 
+
+                    let options = Object.keys(test).filter(key=>test[key].value==this.leaseagrm_id).map(key=>({value: test[key].value, text: test[key].innerText})); 
+
+                    console.log(options); 
+
+
+                    let name = Object.keys(test).filter(key=>test[key].value==this.leaseagrm_id).map(key=>test[key].innerText)[0]; 
+
+                    this.invoice.name = name; 
+
+
+                }
 
             }
         },
@@ -39,13 +75,14 @@ Vue.component
             <div class="container-fluid">
                 <h1>Add New Invoice</h1>
                 <br>
-                <div class="row">
+                <div class="row" ref="leaseagrm_id_select">
                     <select-input
                         name="leaseagrm_id"
                         title="Lease Agreement"
                         overview_controller="leaseagrm"
                         select_value="id"
                         text="name"
+                        @search-data-changed="LeaseagrmIdSelectChanged"
                     ></select-input>
                 </div>
                 <br>
