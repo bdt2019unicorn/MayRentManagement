18fbd67513f7585b759829b652af58c601cc7516
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-09-18 19:48:37 +1200

    solve the issue, easier than I thought

--- server/invoice_controller/test.php
+++ server/invoice_controller/test.php
@@ -12,10 +12,22 @@
          
     "; 
 
-    $result = Connect::TestMultiQuery($sql); 
 
+    $sql = 
+    "
+        INSERT INTO `invoice`(`name`, `leaseagrm_id`) VALUES ('TEST 1', '1'); 
+        SET @invoice_id=LAST_INSERT_ID(); 
+    
+        INSERT INTO `invoice`(`name`, `leaseagrm_id`) VALUES1 (CONCAT(CAST(@invoice_id AS CHAR),' TESTING FOR THE NEW INSERT ID BASY '), '1'); 
+    "; 
 
+    $queries = explode(";",trim($sql)); 
+
+    $result = Connect::ExecTransaction(array_filter($queries)); 
+
+    echo "<pre>"; 
     print_r($result); 
+    echo "</pre>"; 
 
 
 ?>
