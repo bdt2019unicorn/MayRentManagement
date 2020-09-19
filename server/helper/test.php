7dae696d4de8cb230473683557927af29c556888
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-09-19 22:25:38 +1200

    Update test.php
    Former-commit-id: de03bfae7c564a8deda367a7a9d73ae32b20bd3f

--- server/helper/test.php
+++ server/helper/test.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once(realpath(__DIR__."../../../vendor/autoload.php")); 
+    // require_once(realpath(__DIR__."../../../vendor/autoload.php")); 
 
 
     $path = realpath(__DIR__."../../../vendor/autoload.php"); 
