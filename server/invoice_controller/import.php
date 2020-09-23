f740d14580c2744d6cd9d2d168e90671e0597e3f
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-09-23 22:47:48 +1200

    fix utilities issues
    Former-commit-id: e9b978057960e9a4c396ecbddb5b7fa4b68a838e [formerly 307b08ec7e9d425a5e90dd12ae0c829f57ddb3eb]
    Former-commit-id: d39eea42e1d71de6bc51a83925f62eac27894a4c

--- server/invoice_controller/import.php
+++ server/invoice_controller/import.php
@@ -2,7 +2,7 @@
     require_once("../helper/database.php"); 
     $invoices = json_decode($_POST["invoices"], true); 
     $queries = []; 
-    array_push($queries, Query::Insert("invoice", $invoices["invoice"]), "SET @invoice_id=LAST_INSERT_ID();"); 
+    array_push($queries, Query::Insert("invoices", $invoices["invoice"]), "SET @invoice_id=LAST_INSERT_ID();"); 
 
     foreach ($invoices["details"] as $table => $values) 
     {
