c344f23986e6606d6b345bb3fbd5e8d68343c875
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-10-07 22:50:05 +1300

    final work for the overview query - need to put into the production
    Former-commit-id: f8190fc29886545ab40376f41c6d7d5883c6a7af

--- server/overview_controller/overview.php
+++ server/overview_controller/overview.php
@@ -154,6 +154,172 @@
 		$CheckElementInArray($row, $table); 
 	}
 	echo json_encode($table);
+
+
+
+
+	/*
+
+SELECT 
+	`id` AS `leaseid`, 
+	`Tenant_ID` AS `tenantid`, 
+	`apartment_id`, 
+    `Start_date`, 
+    `Finish`, 
+    `Deposit_payment_date`, 
+    (SELECT CONCAT(`tenant`.`First_Name`,' ',`tenant`.`Middle_Name`,' ',`tenant`.`Last_Name`) FROM `tenant` WHERE `tenant`.`id`=`leaseagrm`.`Tenant_ID`) AS `Tenant Name`
+FROM `leaseagrm` 
+WHERE `Finish`>=CURRENT_DATE
+
+
+SELECT 
+	`id` AS `leaseid`, 
+	`Tenant_ID` AS `tenantid`, 
+	`apartment_id`, 
+    `Start_date`, 
+    `Finish`, 
+	`Deposit_payment_date`, 
+	    (
+        CASE 
+        	WHEN `Start_date`<CURRENT_DATE THEN DATE_FORMAT(`Finish`, '%M %d, %Y')
+        	ELSE CONCAT('Move in on ', DATE_FORMAT(`Start_date`, '%M %d, %Y'))
+        END
+    ) AS `Rent Until`, 
+    (
+        CASE 
+        	WHEN `Start_date`<CURRENT_DATE THEN CONCAT('2', DATE_FORMAT(`Finish`, '%Y%m%d'))
+        	ELSE CONCAT('1', DATE_FORMAT(`Start_date`, '%Y%m%d'))
+        END
+    ) AS `Rent Until Value`, 
+    (SELECT CONCAT(`tenant`.`First_Name`,' ',`tenant`.`Middle_Name`,' ',`tenant`.`Last_Name`) FROM `tenant` WHERE `tenant`.`id`=`leaseagrm`.`Tenant_ID`) AS `Tenant Name`
+FROM `leaseagrm` 
+WHERE `Finish`>=CURRENT_DATE
+
+
+
+CREATE TEMPORARY TABLE IF NOT EXISTS `leaseagrm_overview_temp` AS 
+(
+
+    SELECT 
+        `id` AS `leaseid`, 
+        `Tenant_ID` AS `tenantid`, 
+        `apartment_id`, 
+        `Start_date`, 
+        `Finish`, 
+        `Deposit_payment_date`, 
+            (
+            CASE 
+                WHEN `Start_date`<CURRENT_DATE THEN DATE_FORMAT(`Finish`, '%M %d, %Y')
+                ELSE CONCAT('Move in on ', DATE_FORMAT(`Start_date`, '%M %d, %Y'))
+            END
+        ) AS `Rental Status`, 
+        (
+            CASE 
+                WHEN `Start_date`<CURRENT_DATE THEN CONCAT('2', DATE_FORMAT(`Finish`, '%Y%m%d'))
+                ELSE CONCAT('1', DATE_FORMAT(`Start_date`, '%Y%m%d'))
+            END
+        ) AS `Rental Status Value`, 
+        (
+            SELECT IFNULL(SUM(`revenue`.`Amount`),0) FROM `revenue` WHERE `revenue`.`leaseagrm_id` = `id`
+        ) AS `paid_amount`, 
+        (
+            SELECT IFNULL(SUM(`invoice_leaseagrm`.`amount`), 0) FROM `invoice_leaseagrm` 
+            WHERE `invoice_leaseagrm`.`invoice_id` IN (SELECT `invoices`.`id` FROM `invoices` WHERE `invoices`.`leaseagrm_id` = `leaseagrm`.`id`)
+        ) AS `invoice_leaseagrm_amount`, 
+        (
+            SELECT IFNULL(SUM(`invoice_utilities`.`amount`), 0) FROM `invoice_utilities` 
+            WHERE `invoice_utilities`.`invoice_id` IN (SELECT `invoices`.`id` FROM `invoices` WHERE `invoices`.`leaseagrm_id` = `leaseagrm`.`id`)
+        ) AS `invoice_utilities_amount`, 
+        (SELECT CONCAT(`tenant`.`First_Name`,' ',`tenant`.`Middle_Name`,' ',`tenant`.`Last_Name`) FROM `tenant` WHERE `tenant`.`id`=`Tenant_ID`) AS `Tenant Name`
+    FROM `leaseagrm` 
+    WHERE `Finish`>=CURRENT_DATE
+); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TEMPORARY TABLE IF NOT EXISTS `leaseagrm_overview_temp` AS 
+(
+
+    SELECT 
+        `id` AS `leaseid`, 
+		`Tenant_ID` AS `tenantid`, 
+		`ocupants_ids`, 
+        `apartment_id`, 
+        `Start_date`, 
+        `Finish`, 
+        `Deposit_payment_date`, 
+            (
+            CASE 
+                WHEN `Start_date`<CURRENT_DATE THEN DATE_FORMAT(`Finish`, '%M %d, %Y')
+                ELSE CONCAT('Move in on ', DATE_FORMAT(`Start_date`, '%M %d, %Y'))
+            END
+        ) AS `Rental Status`, 
+        (
+            CASE 
+                WHEN `Start_date`<CURRENT_DATE THEN CONCAT('2', DATE_FORMAT(`Finish`, '%Y%m%d'))
+                ELSE CONCAT('1', DATE_FORMAT(`Start_date`, '%Y%m%d'))
+            END
+        ) AS `Rental Status Value`, 
+        (
+            SELECT IFNULL(SUM(`revenue`.`Amount`),0) FROM `revenue` WHERE `revenue`.`leaseagrm_id` = `id`
+        ) AS `paid_amount`, 
+        (
+            SELECT IFNULL(SUM(`invoice_leaseagrm`.`amount`), 0) FROM `invoice_leaseagrm` 
+            WHERE `invoice_leaseagrm`.`invoice_id` IN (SELECT `invoices`.`id` FROM `invoices` WHERE `invoices`.`leaseagrm_id` = `leaseagrm`.`id`)
+        ) AS `invoice_leaseagrm_amount`, 
+        (
+            SELECT IFNULL(SUM(`invoice_utilities`.`amount`), 0) FROM `invoice_utilities` 
+            WHERE `invoice_utilities`.`invoice_id` IN (SELECT `invoices`.`id` FROM `invoices` WHERE `invoices`.`leaseagrm_id` = `leaseagrm`.`id`)
+        ) AS `invoice_utilities_amount`, 
+        (SELECT CONCAT(`tenant`.`First_Name`,' ',`tenant`.`Middle_Name`,' ',`tenant`.`Last_Name`) FROM `tenant` WHERE `tenant`.`id`=`Tenant_ID`) AS `Tenant Name`
+    FROM `leaseagrm` 
+    WHERE `Finish`>=CURRENT_DATE
+); 
+
+CREATE TEMPORARY TABLE `leaseagrm_overview` AS 
+(
+   SELECT 
+		*, 
+		(
+			CASE
+				WHEN `paid_amount` = 0 THEN IF(ISNULL(`Deposit_payment_date`), NULL, 'Deposit paid')
+				ELSE `paid_amount` - `invoice_leaseagrm_amount` - `invoice_utilities_amount`
+			END
+		) AS `Payment Status`, 
+		(`paid_amount` - `invoice_leaseagrm_amount` - `invoice_utilities_amount`) AS `Payment Status Value`
+	FROM `leaseagrm_overview_temp`
+); 
+
+SELECT 
+	`apartment`.`id` AS `ID`, 
+	`apartment`.`name` AS `Apartment Name`, 
+	IFNULL(`leaseagrm_overview`.`Rental Status`, 'Vacant') AS `Rental Status`,
+	IFNULL(`leaseagrm_overview`.`Rental Status Value`, 0) AS `Rental Status Value`, 
+	`leaseagrm_overview`.`Payment Status`, 
+	`leaseagrm_overview`.`Payment Status Value`, 
+	`leaseagrm_overview`.`Tenant Name`, 
+	IFNULL(`leaseagrm_overview`.`ocupants_ids`, '[]') AS `ocupants_ids`, 
+	`leaseagrm_overview`.`leaseid`, 
+	`leaseagrm_overview`.`tenantid`	
+FROM `apartment` LEFT JOIN `leaseagrm_overview` ON `apartment`.`id` = `leaseagrm_overview`.`apartment_id`
+WHERE `apartment`.`building_id` = '1'
+ORDER BY `apartment`.`id`; 
+
+
+
+	*/
 ?>
 
  
