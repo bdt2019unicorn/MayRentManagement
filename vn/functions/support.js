63b0c20f1a122eccd8e0c2c6d85230ac92337f51
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-10-21 11:09:25 +1300

    tidy up all the comments
    Former-commit-id: c4f562dc8b19b46ec2f118d51c5dbfc7a19099d7

--- vn/functions/support.js
+++ vn/functions/support.js
@@ -1,12 +1,3 @@
-/*
-function BindFucntions(component)
-{
-    let support_functions = new component.SupportFunction(component); 
-    Object.keys(support_functions).forEach(func=>component[func] = support_functions[func].bind(component)); 
-}
-
-*/
-
 function BindFucntionsTest(component)
 {
     Object.keys(component.Methods).forEach(func=>component[func] = component.Methods[func].bind(component)); 
