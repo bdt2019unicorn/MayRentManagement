908bed0f89e56b0a21e5c45f49a422156c702071
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-10-20 16:49:50 +1300

    pull all the excel stuff out and get ready for another one for test
    Former-commit-id: a0434390b20b4d754b5c6b94ff6ad9ba97a9370c

--- vn/functions/support.js
+++ vn/functions/support.js
@@ -1,5 +1,5 @@
 function BindFucntions(component)
 {
-    let support_functions = new component.SupportFunction(); 
+    let support_functions = new component.SupportFunction(component); 
     Object.keys(support_functions).forEach(func=>component[func] = support_functions[func].bind(component)); 
 }
