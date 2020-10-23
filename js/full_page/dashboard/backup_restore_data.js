ac1b5d93f3c9a54c8bce8885fe61148b2c1fc008
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-10-23 19:31:40 +1300

    final change and all split in the game

---
+++ js/full_page/dashboard/backup_restore_data.js
@@ -0,0 +1,61 @@
+Vue.component
+(
+    "backup-data", 
+    {
+        components: {...bootstrap}, 
+        template: 
+        `
+            <b-button variant="link" href="#">
+                <b-icon-cloud-upload></b-icon-cloud-upload>
+                Download back up file
+            </b-button>
+        `
+    }
+); 
+
+Vue.component
+(
+    "restore-data", 
+    {
+        data() 
+        {
+            return {
+                file: undefined         
+            }
+        },
+        components: {...vueFragment}, 
+        template: 
+        `
+            <fragment> 
+                <vs-upload 
+                    limit="1" 
+                    :show-upload-button="false" 
+                    text="Upload the restore File" 
+                    @change="file = arguments[1][arguments[1].length-1]" 
+                    @on-delete="file=undefined" 
+                />
+                <br>
+                <submit-button v-if="file" title="Backup data"></submit-button>
+            </fragment>
+        `
+    }
+); 
+
+Vue.component
+(
+    "backup-restore-data", 
+    {
+        components: {...bootstrap}, 
+        template: 
+        `
+            <b-tabs content-class="mt-3" justified vertical pills lazy nav-wrapper-class="col-4" nav-class="text-center" v-model="backup_restore_tab">
+                <b-tab title="Backup">
+                    <backup-data></backup-data>
+                </b-tab>
+                <b-tab title="Restore">
+                    <restore-data></restore-data>
+                </b-tab>
+            </b-tabs>
+        `
+    }
+); 
