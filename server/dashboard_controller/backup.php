9c086ffd50d198e18a1a664fd12676f8ef9eb68e
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-10-24 02:27:18 +1300

    couple work to get done but make sure you do some tidy up - just a little

--- server/dashboard_controller/backup.php
+++ server/dashboard_controller/backup.php
@@ -1,4 +1,54 @@
 <?php 
+    require_once("../helper/database.php"); 
+    // $backup = new Backup(); 
+    // $backup->BackupDatabase(); 
 
+    // header('Content-Type: application/sql');
+    // header('Content-Disposition: attachment; filename=may_backup_database.sql');
+
+    $sql = 
+    "
+        SET @db=DATABASE(); 
+        SELECT `TABLE_NAME` AS `table_name`, CONCAT('SELECT * FROM `', `TABLE_NAME`, '`;') AS `query`
+        FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA` = @db; 
+        SELECT DATABASE(); 
+    ". Query::SelectData("information_schema`.`REFERENTIAL_CONSTRAINTS", ["DISTINCT `REFERENCED_TABLE_NAME`"]); 
+
+    $tables = Connect::MultiQuery($sql, true); 
+    function AllTables($all)
+    {
+        $tables = []; 
+        $sql = ""; 
+        foreach ($all as $table) 
+        {
+            $sql.= $table["query"]; 
+            array_push($tables, $table["table_name"]); 
+        }
+        $data = Connect::MultiQuery($sql, true); 
+        $result = []; 
+        foreach ($tables as $index => $table) 
+        {
+            $result[$table] = []; 
+            $table_data = $data[$index]; 
+            foreach ($table_data as $value) 
+            {
+                array_push($result[$table], Query::Insert($table, $value)); 
+            }
+        }
+        
+        return $result; 
+    }
+    
+    $tables = 
+    [
+        "all"=> AllTables($tables[0]), 
+        "references"=>$tables[2]
+    ]; 
+
+    // echo $sql; 
+
+    echo '<pre>'; 
+    print_r($tables); 
+    echo '</pre>'; 
 
 ?>
