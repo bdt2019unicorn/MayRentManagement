15cf494a189e3a34a18f992316d3d785e4c00d9a
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-02 13:46:59 +1300

    put the download and the reset on the game
    Former-commit-id: 24b06ea2450e5068c8ef3d6affbf9c662b7798f4 [formerly 6c9c6690ff0a45eb0254ff9947d558dfb587140e]
    Former-commit-id: 76352d48345bf9ec376063ae13e5414b826940d5

--- server/excel_controller/download_test.php
+++ server/excel_controller/download_test.php
@@ -1,6 +1,6 @@
 <?php
-    header("Location: hello world.xlsx"); 
-    header("Content-disposition: attachment; filename=hello world.xlsx"); 
+    header("Location: templates/hello world.xlsx"); 
+    header("Content-disposition: attachment; filename=templates/hello world.xlsx"); 
 
 
 ?>
