50a435d0d125b447e7da86c238c69d5418877fbb
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-02 18:04:17 +1300

    Update unit.php
    Former-commit-id: 605dbd057765599eeae70bf199b841f972aec96a

--- server/excel_controller/unit.php
+++ server/excel_controller/unit.php
@@ -40,6 +40,63 @@
         $sheet->duplicateStyle($instruction_styles, "A1:A{$config['total_row']}"); 
     }
 
+    function ColumnHeaders()
+    {
+        global $sheet; 
+        global $params; 
+        global $col; 
+        global $instruction_titles; 
+        global $instruction_styles; 
+
+        foreach ($params as $column => $param) 
+        {
+            $col++; 
+            $char = chr($col); 
+            $sheet->getColumnDimension($char)->setAutoSize(true); 
+            $sheet->setCellValue("{$char}1", $column); 
+            $styles = 
+            [
+                "alignment"=>
+                [
+                    'horizontal' => Style\Alignment::HORIZONTAL_CENTER 
+                ], 
+                "borders"=> 
+                [
+                    "allBorders"=>
+                    [
+                        'borderStyle' => Style\Border::BORDER_THIN,
+                    ]
+                ]
+            ]; 
+            foreach ($param as $key => $value) 
+            {
+                $cell = "{$char}{$instruction_titles[$key]}"; 
+                $sheet->setCellValue($cell, $value); 
+                $sheet->getStyle($cell)->applyFromArray($styles); 
+            }
+        }
+    
+    
+    
+        $last_char = chr($col); 
+    
+        $sheet->duplicateStyle($instruction_styles, "A1:{$last_char}1");
+    }
+
+    function SeparateInstructionsData()
+    {
+        global $sheet; 
+        global $col; 
+        global $instruction_titles; 
+
+        $last_char = chr($col); 
+        $merge_styles = MergeStyle(); 
+        $merge_range = "B{$instruction_titles['Separate']}:{$last_char}{$instruction_titles['Separate']}"; 
+        $sheet->duplicateStyle($merge_styles, $merge_range); 
+        $sheet->mergeCells($merge_range); 
+    }
+
+
     
     $instruction_titles = $config["instruction_titles"]; 
     $instruction_styles = CellStyles(); 
@@ -47,44 +104,8 @@
 
     
     $col = ord("A"); 
-    foreach ($params as $column => $param) 
-    {
-        $col++; 
-        $char = chr($col); 
-        $sheet->getColumnDimension($char)->setAutoSize(true); 
-        $sheet->setCellValue("{$char}1", $column); 
-        $styles = 
-        [
-            "alignment"=>
-            [
-                'horizontal' => Style\Alignment::HORIZONTAL_CENTER 
-            ], 
-            "borders"=> 
-            [
-                "allBorders"=>
-                [
-                    'borderStyle' => Style\Border::BORDER_THIN,
-                ]
-            ]
-        ]; 
-        foreach ($param as $key => $value) 
-        {
-            $cell = "{$char}{$instruction_titles[$key]}"; 
-            $sheet->setCellValue($cell, $value); 
-            $sheet->getStyle($cell)->applyFromArray($styles); 
-        }
-    }
-
-
-
-    $last_char = chr($col); 
-
-    $sheet->duplicateStyle($instruction_styles, "A1:{$last_char}1");
-
-    $merge_styles = MergeStyle(); 
-    $merge_range = "B{$instruction_titles['Separate']}:{$last_char}{$instruction_titles['Separate']}"; 
-    $sheet->duplicateStyle($merge_styles, $merge_range); 
-    $sheet->mergeCells($merge_range); 
+    ColumnHeaders(); 
+    SeparateInstructionsData(); 
 
     $writer = new Xlsx($spreadsheet);
 
