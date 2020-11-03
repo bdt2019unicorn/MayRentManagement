fdc25103d6871667471149a9bf56b84a926d8a38
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-04 11:12:58 +1300

    break it to classes - need more work on this
    Former-commit-id: 51946359474c0b338a0354a0ec81f7e9a4732a69

---
+++ server/excel_controller/ExcelSpreadSheet.php
@@ -0,0 +1,142 @@
+<?php     
+    require_once("../helper/database.php"); 
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet; 
+    use PhpOffice\PhpSpreadsheet\NamedRange; 
+    use PhpOffice\PhpSpreadsheet\Cell\DataValidation; 
+
+    require_once("SampleCellStyles.php"); 
+
+    class ExcelSpreadSheet
+    {
+        public $spreadsheet; 
+        private $config; 
+        private $sample_cell_styles; 
+        private $instruction_titles; 
+        private $instruction_styles; 
+        
+        private $sheet_title = "Data"; 
+        private $sheet; 
+
+        private $params; 
+
+        private $column_asc; 
+
+        function __construct($controller)
+        {
+            $this->spreadsheet = new Spreadsheet();
+            $this->sheet = $this->spreadsheet->getActiveSheet();
+            $this->sheet->setTitle($this->sheet_title); 
+        
+            $config = file_get_contents("config.json"); 
+            $this->config = json_decode($config, true); 
+        
+            $this->sample_cell_styles = new SampleCellStyles(); 
+            $this->instruction_titles = $this->config["instruction_titles"]; 
+            $this->instruction_styles = $this->sample_cell_styles->CellStyles("Instruction Title"); 
+
+            $params = file_get_contents("params/{$controller}.json"); 
+            $this->params = json_decode($params, true); 
+
+
+            $this->column_asc = ord("A"); 
+        }
+
+        function GenerateSpreadsheet()
+        {
+            $this->InstructionTitles(); 
+            $this->ColumnHeaders(); 
+            $this->SeparateInstructionsData(); 
+            return $this->spreadsheet; 
+        }
+
+        private function InstructionTitles()
+        {
+            $this->sheet->getColumnDimension("A")->setAutoSize(true); 
+            foreach ($this->instruction_titles as $title => $row) 
+            {
+                $this->sheet->setCellValue("A{$row}", $title); 
+            }
+            $this->sheet->duplicateStyle($this->instruction_styles, "A1:A{$this->config['total_row']}"); 
+        }
+        
+        private function ColumnHeaders()
+        {
+            $format_codes = $this->config["format_codes"]; 
+            $data_row_start = $this->instruction_titles["Separate"] + 1; 
+            foreach ($this->params["columns"] as $column => $param) 
+            {
+                $this->column_asc++; 
+                $char = chr($this->column_asc); 
+                $this->sheet->getColumnDimension($char)->setAutoSize(true); 
+                $this->sheet->setCellValue("{$char}1", $column); 
+
+                $instruction_text_style = $this->sample_cell_styles->CellStyles("Instruction Text"); 
+                foreach ($param as $key => $value) 
+                {
+                    $cell = "{$char}{$this->instruction_titles[$key]}"; 
+                    $this->sheet->setCellValue($cell, $value); 
+                    $this->sheet->duplicateStyle($instruction_text_style, $cell); 
+                }
+
+                $range_style = $this->sample_cell_styles->CellStyles($param["Data type"]); 
+                $range_style->getNumberFormat()->setFormatCode($format_codes[$param["Data type"]]); 
+                $this->sheet->duplicateStyle($range_style, "{$char}{$data_row_start}:{$char}{$this->config['total_row']}"); 
+
+                if(isset($this->params["dropdown_list"][$column]))
+                {
+                    $this->DropdownList($this->params["dropdown_list"][$column], $data_row_start, $char); 
+                }
+            }
+            $last_char = chr($this->column_asc); 
+            $this->sheet->duplicateStyle($this->instruction_styles, "A1:{$last_char}1");
+        }
+        
+        private function DropdownList($dropdown, $data_row_start, $char)
+        {
+            $dropdown_list = Connect::SelectData($dropdown["table"], $dropdown["selects"]); 
+            if(!count($dropdown_list))
+            {
+                return; 
+            }
+
+            $title = null; 
+            $cell_values = []; 
+            foreach ($dropdown_list[0] as $key => $value) 
+            {
+                $title = $key; 
+                array_push($cell_values, $title, $value); 
+                break; 
+            }
+            for ($index=1; $index<count($dropdown_list); $index++) 
+            { 
+                array_push($cell_values, $dropdown_list[$index][$title]); 
+            }
+
+            $end_range = count($cell_values); 
+            $dropdown_sheet = new Worksheet($this->spreadsheet, $title); 
+            $dropdown_sheet->getColumnDimension("A")->setAutoSize(true); 
+            $dropdown_sheet->fromArray(array_chunk($cell_values, 1)); 
+            $this->spreadsheet->addNamedRange(new NamedRange($title, $dropdown_sheet, "\$A\$2:\$A\${$end_range}")); 
+            $this->spreadsheet->addSheet($dropdown_sheet); 
+            $dropdown_sheet->getStyle("A1")->applyFromArray($this->config["dropdown_headings_styles"]); 
+
+            for ($row=$data_row_start; $row <=$this->config['total_row']; $row++) 
+            { 
+                $validation = $this->sheet->getCell("{$char}{$row}")->getDataValidation();
+                $validation->setType(DataValidation::TYPE_LIST); 
+                $validation->setFormula1("={$title}"); 
+                $validation->setShowDropDown(true); 
+            }
+        }
+
+        private function SeparateInstructionsData()
+        {
+            $last_char = chr($this->column_asc); 
+            $merge_styles = $this->sample_cell_styles->CellStyles("Merge"); 
+            $merge_range = "B{$this->instruction_titles['Separate']}:{$last_char}{$this->instruction_titles['Separate']}"; 
+            $this->sheet->duplicateStyle($merge_styles, $merge_range); 
+            $this->sheet->mergeCells($merge_range); 
+        }
+    }
+?>
--- server/excel_controller/test.php
+++ server/excel_controller/test.php
@@ -6,49 +6,55 @@
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
     use PhpOffice\PhpSpreadsheet\Cell\DataValidation; 
 
-    $data = Connect::GeneralData("tenant"); 
+    // $data = Connect::GeneralData("tenant"); 
+
+    require_once("SampleCellStyles.php"); 
 
     $spreadsheet = new Spreadsheet();
     $sheet = $spreadsheet->getActiveSheet();
     $sheet->setTitle("First Sheet"); 
 
 
-    $col_count = 0; 
 
-    for ($i=0; $i < count($data) ; $i++) 
-    { 
-        // $cell = $i+1; 
-        $column = ord("A")-1;
 
-        foreach ($data[$i] as $key => $value) 
-        {
-            $column++; 
-            $cell = chr($column); 
-            if($i==0)
-            {
-                $col_count = count($data[$i]); 
-                $sheet->setCellValue($cell."1", $key); 
-            }
+
+
+    // $col_count = 0; 
+
+    // for ($i=0; $i < count($data) ; $i++) 
+    // { 
+    //     // $cell = $i+1; 
+    //     $column = ord("A")-1;
+
+    //     foreach ($data[$i] as $key => $value) 
+    //     {
+    //         $column++; 
+    //         $cell = chr($column); 
+    //         if($i==0)
+    //         {
+    //             $col_count = count($data[$i]); 
+    //             $sheet->setCellValue($cell."1", $key); 
+    //         }
             
-            $sheet->setCellValue($cell.strval($i+2), $value); 
-        } 
-    }
+    //         $sheet->setCellValue($cell.strval($i+2), $value); 
+    //     } 
+    // }
 
 
-    $range = "=\$A\$1:\$". chr(ord("A")-1+$col_count)."\$". strval(count($data)+1); 
+    // $range = "=\$A\$1:\$". chr(ord("A")-1+$col_count)."\$". strval(count($data)+1); 
 
     // echo $range; 
 
-    $spreadsheet->addNamedRange(new NamedRange("tenants", $sheet, $range)); 
+    // $spreadsheet->addNamedRange(new NamedRange("tenants", $sheet, $range)); 
 
-    $second_sheet = new Worksheet($spreadsheet, "Second Sheet"); 
-    $validation = $second_sheet->getCell('A1')->getDataValidation();
-    $validation->setType(DataValidation::TYPE_LIST); 
-    $row_end = count($data) + 1; 
-    $validation->setFormula1("='First Sheet'!B2:B{$row_end}"); 
-    $validation->setShowDropDown(true); 
+    // $second_sheet = new Worksheet($spreadsheet, "Second Sheet"); 
+    // $validation = $second_sheet->getCell('A1')->getDataValidation();
+    // $validation->setType(DataValidation::TYPE_LIST); 
+    // $row_end = count($data) + 1; 
+    // $validation->setFormula1("='First Sheet'!B2:B{$row_end}"); 
+    // $validation->setShowDropDown(true); 
 
-    $spreadsheet->addSheet($second_sheet); 
+    // $spreadsheet->addSheet($second_sheet); 
 
     // $sheet->setCellValue('A1', 'Hello World !');
 
@@ -59,9 +65,36 @@
         mkdir($directory);    
     }
 
-    $writer->save('templates/hello world.xlsx');
+    $writer->save('templates/test.xlsx');
 
     // echo '<pre>'; 
     // print_r($data); 
     // echo '</pre>'; 
+
+
+    class Outer
+    {
+        function __construct()
+        {
+            echo "<h1>contruct</h1>"; 
+        }
+
+        // private class Inner
+        // {
+        //     function __construct()
+        //     {
+        //         echo "<h1 style='color:red;'>inner</h1>"; 
+        //     }
+        // }
+
+        public function Test()
+        {
+            // $inner = new Inner(); 
+            echo "<h2>Inner created</h2>"; 
+        }
+    }
+
+
+    $outer = new Outer(); 
+    $outer->Test(); 
 ?>
