430a82bdbd1155f957ebdeddcaf667d47392a286
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-04 12:02:25 +1300

    innitialize the leaseagrm - need more work soon
    Former-commit-id: 13ca89250f9afbb5c9b8a27647cfa548906daff7 [formerly 68b399e1b5470d9b67fae50d3acf0d8afd48b8c1]
    Former-commit-id: 415efa73927f01bc18a473d49a16cc277aaae157

--- server/excel_controller/leaseagrm_test.php
+++ server/excel_controller/leaseagrm_test.php
@@ -1,6 +1,26 @@
 <?php 
+    require_once("ExcelSpreadSheet.php"); 
 
+    use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+    $controller = "leaseagrm"; 
 
+    function ExportToFile($spreadsheet)
+    {
+        $writer = new Xlsx($spreadsheet);
 
+        $directory = "templates/"; 
+        if(!file_exists($directory))
+        {
+            mkdir($directory);    
+        }
+    
+        $writer->save('templates/leaseagrm_test_php.xlsx');
+    }
+
+    $excel_spreadsheet = new ExcelSpreadSheet($controller); 
+
+    $spreadsheet = $excel_spreadsheet->GenerateSpreadsheet(); 
+
+    ExportToFile($spreadsheet); 
 ?>
