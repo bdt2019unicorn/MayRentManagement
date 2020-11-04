261d69b3acdbe63fc5a3c7ad5ba3f5ffd835b33e
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-04 14:48:53 +1300

    get all the json done

--- server/excel_controller/test.php
+++ server/excel_controller/test.php
@@ -1,100 +1,28 @@
 <?php 
-    require_once("../helper/database.php"); 
-    use PhpOffice\PhpSpreadsheet\Spreadsheet;
-    use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet; 
-    use PhpOffice\PhpSpreadsheet\NamedRange; 
+
+    require_once("ExcelSpreadSheet.php"); 
+
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-    use PhpOffice\PhpSpreadsheet\Cell\DataValidation; 
 
-    // $data = Connect::GeneralData("tenant"); 
+    $controller = "buildings"; 
 
-    require_once("SampleCellStyles.php"); 
-
-    $spreadsheet = new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-    $sheet->setTitle("First Sheet"); 
-
-
-
-
-
-
-    // $col_count = 0; 
-
-    // for ($i=0; $i < count($data) ; $i++) 
-    // { 
-    //     // $cell = $i+1; 
-    //     $column = ord("A")-1;
-
-    //     foreach ($data[$i] as $key => $value) 
-    //     {
-    //         $column++; 
-    //         $cell = chr($column); 
-    //         if($i==0)
-    //         {
-    //             $col_count = count($data[$i]); 
-    //             $sheet->setCellValue($cell."1", $key); 
-    //         }
-            
-    //         $sheet->setCellValue($cell.strval($i+2), $value); 
-    //     } 
-    // }
-
-
-    // $range = "=\$A\$1:\$". chr(ord("A")-1+$col_count)."\$". strval(count($data)+1); 
-
-    // echo $range; 
-
-    // $spreadsheet->addNamedRange(new NamedRange("tenants", $sheet, $range)); 
-
-    // $second_sheet = new Worksheet($spreadsheet, "Second Sheet"); 
-    // $validation = $second_sheet->getCell('A1')->getDataValidation();
-    // $validation->setType(DataValidation::TYPE_LIST); 
-    // $row_end = count($data) + 1; 
-    // $validation->setFormula1("='First Sheet'!B2:B{$row_end}"); 
-    // $validation->setShowDropDown(true); 
-
-    // $spreadsheet->addSheet($second_sheet); 
-
-    // $sheet->setCellValue('A1', 'Hello World !');
-
-    $writer = new Xlsx($spreadsheet);
-    $directory = "templates/"; 
-    if(!file_exists($directory))
+    function ExportToFile($spreadsheet)
     {
-        mkdir($directory);    
-    }
+        $writer = new Xlsx($spreadsheet);
 
-    $writer->save('templates/test.xlsx');
-
-    // echo '<pre>'; 
-    // print_r($data); 
-    // echo '</pre>'; 
-
-
-    class Outer
-    {
-        function __construct()
+        $directory = "templates/"; 
+        if(!file_exists($directory))
         {
-            echo "<h1>contruct</h1>"; 
+            mkdir($directory);    
         }
 
-        // private class Inner
-        // {
-        //     function __construct()
-        //     {
-        //         echo "<h1 style='color:red;'>inner</h1>"; 
-        //     }
-        // }
-
-        public function Test()
-        {
-            // $inner = new Inner(); 
-            echo "<h2>Inner created</h2>"; 
-        }
+        $writer->save('templates/buildings.xlsx');
     }
 
+    $excel_spreadsheet = new ExcelSpreadSheet($controller); 
 
-    $outer = new Outer(); 
-    $outer->Test(); 
+    $spreadsheet = $excel_spreadsheet->GenerateSpreadsheet(); 
+
+    ExportToFile($spreadsheet); 
+
 ?>
