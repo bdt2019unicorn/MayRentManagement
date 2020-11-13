56c18590e9c87039da3200986f59288453896751
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-13 20:08:40 +1300

    need to do quick work on the duplicate of the dashboard
    Former-commit-id: c6c1137a63dd7821fa61f57342b586ce53c1fbb2

--- js/mixins/table_actions.js
+++ js/mixins/table_actions.js
@@ -9,10 +9,23 @@ var table_actions_mixin =
 
     methods: 
     {
+        ExcelSheet(table_data, hidden_columns=[])
+        {
+            let data = table_data.map 
+            (
+                row=>
+                {
+                    let modify = {}; 
+                    Object.keys(row).filter(column=>!hidden_columns.includes(column)).forEach(column=>modify[column]=row[column]); 
+                    return modify; 
+                }
+            ); 
+            return XLSX.utils.json_to_sheet(data); 
+        }, 
         IdCheckChanged(selected_rows, filter_by)
         {
             this.check_array = selected_rows.map(row=>row[filter_by]); 
-        },     
+        } 
     },
 
 }
