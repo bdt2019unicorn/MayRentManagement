3647996952341c059179b50a8814f3c7ea6601f3
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-15 11:48:20 +1300

    INNITIALIZE THE WORK
    Former-commit-id: 0963323b384b8bc2b7e6c0ce313d675fd64ad93b

---
+++ js/full_page/page-wrapper/resolve_old_lease.js
@@ -0,0 +1,27 @@
+Vue.component
+(
+    "resolve-old-lease", 
+    {
+        mixins: [support_mixin], 
+        data() 
+        {
+            return {
+                
+            }
+        },
+        created() 
+        {
+            
+        },
+        methods: 
+        {
+
+        },
+        template: 
+        `
+            <div>
+                <h1>Test</h1>
+            </div>
+        `
+    }
+); 
---
+++ server/overview_controller/resolve_old_contract.php
@@ -0,0 +1,40 @@
+<?php 
+    require_once("../helper/database.php"); 
+    $commands = 
+    [
+        "LoadOldLeases"=> function()
+        {
+            $sql = 
+            "
+                SELECT 
+                    *, 
+                    GREATEST
+                    (
+                        `Start_date`, 
+                        LAST_DAY(CURRENT_DATE - INTERVAL 1 MONTH)
+                    ) AS `date_charged_until`
+                FROM `leaseagrm` 
+                WHERE 
+                    `id` NOT IN (SELECT DISTINCT `leaseagrm_id` FROM `invoices`) AND 
+                    `unit_id` IN (SELECT `id` FROM `unit` WHERE `unit`.`building_id` = '{$_GET['building_id']}') AND
+                    CURRENT_DATE BETWEEN `Start_date` AND `Finish`
+            "; 
+
+            $data = Connect::GetData($sql); 
+            echo '<pre>'; 
+            print_r($data);
+            echo '</pre>'; 
+        }
+    ]; 
+
+    try 
+    {
+        $commands[$_GET["command"]](); 
+    }
+    catch (\Throwable $throwable)
+    {
+        echo false; 
+    }
+    
+
+?>
