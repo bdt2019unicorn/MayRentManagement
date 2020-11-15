2919d6d48eef141266553f65113f7142407ac01f
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-15 16:11:33 +1300

    need to get a date time thing for this

--- js/full_page/page-wrapper/resolve_old_lease.js
+++ js/full_page/page-wrapper/resolve_old_lease.js
@@ -6,21 +6,46 @@ Vue.component
         data() 
         {
             return {
-                
+                old_leases: []
             }
         },
         created() 
         {
-            
+            this.LoadOldLeases(); 
         },
         methods: 
         {
-
+            LoadOldLeases()
+            {
+                let url = `server/overview_controller/resolve_old_contract.php?command=LoadOldLeases&building_id=${this.$route.params.building_id}`; 
+                let data = this.AjaxRequest(url); 
+                try 
+                {
+                    this.old_leases = JSON.parse(data); 
+                }
+                catch
+                {
+                    this.old_leases = []; 
+                }
+            }
         },
         template: 
         `
-            <div>
-                <h1>Test</h1>
+            <div class="container-fluid">
+                <h1>Old Contracts To Resolve Payment</h1>
+                <template v-if="old_leases.length">
+                    
+                </template>
+                <template v-else>
+                    <br>
+                    <vs-row vs-align="center" vs-justify="center" vs-type="flex">
+                        <vs-col vs-w="6">
+                            <div class="border border-warning">
+                                <h3 class="text-danger text-center">There are currently no unresolved contract at this building</h3>
+                            </div>
+                        </vs-col>
+                    </vs-row>
+                </template>
             </div>
         `
     }
