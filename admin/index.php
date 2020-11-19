ffdc3f7d0889ef73fa7e3918f9327e14a6177ce2
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-20 01:25:45 +1300

    login and authorize sorted
    Former-commit-id: 743588ac46b8bef6225704bad7ddd2134b815dbe

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,23 @@
-<?php 
 
-?>
+<html>
+    <head>
+        <?php include("layout/head.php") ?>
+    </head>
+    <body>
+        <?php 
+            require_once("../server/helper/database.php"); 
+            $tables = Connect::GetData("SHOW TABLES"); 
+            echo '<pre>'; 
+            print_r($tables); 
+            echo '</pre>'; 
+        ?>
+
+        <footer>
+            <?php include("layout/footer.php") ?>
+            <script src="js/index.js"></script>
+        </footer>
+    </body>
+    
 
 
-<h1>TEst</h1>
+</html>
---
+++ admin/js/check-user.js
@@ -0,0 +1,21 @@
+function CheckUser()
+{
+    var username = sessionStorage.getItem("username"); 
+    var id = sessionStorage.getItem("user_id"); 
+    if(username && id)
+    {
+        var data = 
+        {
+            username: username, 
+            id: id, 
+            approved: 1 
+        }; 
+        let url = "../server/admin_database.php?command=CheckUser"; 
+        let result = support_mixin.methods.SubmitData("check_user", url, data); 
+        return Boolean(result); 
+    }
+    else 
+    {
+        return false; 
+    }
+}
