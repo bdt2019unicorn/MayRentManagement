bad04ef75a4626a62ae5c70ea1492d8f09f32220
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-22 12:24:32 +1300

    Update overview_queries.php
    Former-commit-id: b20b9915d41e3864b7d828922d36089d859d0836

--- server/helper/overview_queries.php
+++ server/helper/overview_queries.php
@@ -48,19 +48,29 @@
         use OverviewTrait; 
         public static function Selects($edit, $building_id, $id)
         {
+            $full_name = "CONCAT(IFNULL(`Last_Name`,''), ' ', IFNULL(`First_Name`,'')) AS `Full Name`"; 
             return $edit? 
             [
                 "*", 
-                "CONCAT(IFNULL(`Last_Name`,''), ' ', IFNULL(`Middle_Name`,''), ' ', IFNULL(`First_Name`,'')) AS `Full Name`"
+                $full_name
             ]: 
             [
                 "`id` AS `ID`", 
-                "CONCAT(IFNULL(`Last_Name`,''), ' ', IFNULL(`Middle_Name`,''), ' ', IFNULL(`First_Name`,'')) AS `Full Name`", 
-                "`Passport_ID_number` AS `ID Number`"
+                "
+                    (
+                        SELECT `unit`.`name` FROM `unit` 
+                        WHERE `unit`.`id` IN (SELECT `unit_id` FROM `leaseagrm` WHERE `leaseagrm`.`Tenant_ID` = `tenant`.`id`)
+                        LIMIT 1 
+                    ) AS `Unit Name`
+                ", 
+                $full_name, 
+                "`Mobile_Phone` AS `Mobile Phone`", 
+                "`Personal_Email` AS `Personal Email`", 
+                "`Passport_ID_number` AS `Passport/ID number`", 
+                "DATE_FORMAT(`Date_of_birth`, '%d %M %Y') AS `Date of birth`" 
             ]; 
         }
     }
-
 
     class GeneralOverview
     {
