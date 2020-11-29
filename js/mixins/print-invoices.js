17c53fae2c12c5d6ed52d0cc298170385ffee849
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-11-29 18:41:01 +1300

    ✨feature: Able to print the invoice to word .doc file

--- js/mixins/print-invoices.js
+++ js/mixins/print-invoices.js
@@ -10,92 +10,83 @@ var print_invoices_mixin =
         InvoiceHtml(invoice, {footer, image})
         {
             return `
-                <div class="container-fluid mx-3">
-                    <div class="row text-center"><div class="col"><img src="${image}"></div></div>
-                    <h1 class="text-center">RENTAL AND UTILITY CHARGE</h1>
-                    <br>
-                    <div class="row">
-                        <div class="col-1"><b>Date</b></div>
-                        <div class="col-1">:</div>
-                        <div class="col-4">${this.DateReformatDisplay()}</div>
-                        <div class="col-6">ROE:</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-1"><b>Invoice</b></div>
-                        <div class="col-1">:</div>
-                        <div class="col">${invoice.invoice["name"]}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-1"><b>To</b></div>
-                        <div class="col-1">:</div>
-                        <div class="col">${invoice.invoice["tenant"]}<br><b>${invoice.invoice["unit"]}</b></div>
-                    </div>
-                </div>
+                <table style="width: 100%; margin: 3px;"
+                    <tr><td colspan="5" style="text-align: center;"><img src="${image}"></td></tr>
+                    <tr><td colspan="5"><h1 style="text-align: center;">RENTAL AND UTILITY CHARGE</h1></td></tr>
+                    <tr><td><br></td></tr>
+                    <tr>
+                        <td><b>Date</b></td><td>:</td><td>${this.DateReformatDisplay()}</td>
+                        <td>ROE:</td>
+                    </tr>
+                    <tr><td><b>Invoice</b></td><td>:</td><td>${invoice.invoice["name"]}</td></tr>
+                    <tr><td><b>To</b></td><td>:</td><td>${invoice.invoice["tenant"]}<br><b>${invoice.invoice["unit"]}</b></td></tr>
+                </table>
                 <br>
-                <div class="containter-fluid mx-3">
-                    <div class="row text-center">
-                        <div class="col text-center">
-                            <style>
-                                table.w-100 th, td 
-                                {
-                                    border: 3px; 
-                                    border-style: solid;
-                                }
-                            </style>
-                            <table class="w-100">
+                <style>
+                    .description
+                    {
+                        width: 100%;
+                        border-collapse: collapse;
+                    }
+                    .description th, .description td
+                    {
+                        border: 3px;
+                        border-style: solid;
+                    }
+                    .utilities
+                    {
+                        width: 100%;
+                    }
+                    .utilities td
+                    {
+                        border-style: none;
+                    }
+                </style>
+                <table class="description">
+                    <tr>
+                        <th style="text-align: left;">DESCRIPTION</th>
+                        <th style="text-align: right;">VND</th>
+                    </tr>
+                    ${
+                        invoice.details.leaseagrm.map
+                        (
+                            ({amount, name, ...rest})=>`<tr><td style="text-align: left;">${name}</td><td style="text-align: right;">${amount}</td></tr>`
+                        ).join("\n") 
+                    }
+                    ${
+                        invoice.details.utilities.map 
+                        (
+                            ({name, previous_date, date, previous_number, number, amount, ...rest})=>
+                            `
                                 <tr>
-                                    <th>DESCRIPTION</th>
-                                    <th class="text-right">VND</th>
-                                </tr>
-                                ${
-                                    invoice.details.leaseagrm.map
-                                    (
-                                        ({amount, name, ...rest})=>
-                                        `
-                                            <tr>
-                                                <td class="text-left"><div class="container-fluid">${name}</div></td>
-                                                <td class="text-right">${amount}</td>
-                                            </tr>
-                                        `
-                                    ).join("\n") 
-                                }
-                                ${
-                                    invoice.details.utilities.map 
-                                    (
-                                        ({name, previous_date, date, previous_number, number, amount, ...rest})=>
-                                        `
+                                    <td>
+                                        <table class="utilities">
+                                            <tr><td colspan="2">${name}</td></tr>
                                             <tr>
                                                 <td>
-                                                    <div class="container-fluid">
-                                                        <div class="row">
-                                                            <div class="col-12">${name}</div>
-                                                            <div class="col-2">
-                                                                Begining<br>
-                                                                Finishing<br>
-                                                                Total 
-                                                            </div>
-                                                            <div class="col-6">
-                                                                ${previous_date} <br>
-                                                                ${date} <br>
-                                                                ${this.NumeralFormat((Number(number) - Number(previous_number)))} VND/m3
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                                                    Begining<br>
+                                                    Finishing<br>
+                                                    Total 
                                                 </td>
-                                                <td class="text-right"><br><br><br>${amount}</td>
+                                                <td>
+                                                    ${previous_date} <br>
+                                                    ${date} <br>
+                                                    ${this.NumeralFormat((Number(number) - Number(previous_number)))} VND/m3
+                                                </td>
                                             </tr>
-                                        `
-                                    ).join("\n") 
-                                }
-
-                                <tr>
-                                    <th><h5>Grand Total</h5></th>
-                                    <th class="text-right"><h5>${this.NumeralFormat(invoice.invoice["grand_total"])}</h5></th>
+                                        </table>
+                                    </td>
+                                    <td style="text-align: right;"><br><br><br>${amount}</td>
                                 </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                            `
+                        ).join("\n") 
+                    }
+
+                    <tr>
+                        <th style="text-align: left;">Grand Total</th>
+                        <th style="text-align: right;">${this.NumeralFormat(invoice.invoice["grand_total"])}</th>
+                    </tr>
+                </table>
                 <br>
                 ${footer}
                 <br><br>
