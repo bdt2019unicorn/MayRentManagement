fec430a23752a6ab2dca32de4be59e5679e3d413
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-12-01 22:03:56 +1300

    Update PrintInvoices.php

--- server/invoice_controller/print_invoices/PrintInvoices.php
+++ server/invoice_controller/print_invoices/PrintInvoices.php
@@ -68,9 +68,9 @@
         
         private function Base64Logo()
         {
-            $path = realpath(__DIR__ . "/../../../../img/logo.jpeg"); 
-            $test = realpath(__DIR__ ."/../.."); 
-            echo "<h1>Path is : $test</h1>"; 
+            $path = realpath(__DIR__ . "/../../../img/logo.jpeg"); 
+            $test = realpath(__DIR__ ."/../../../"); 
+            echo "<h1>Path is : $path</h1>"; 
             $image = file_get_contents($path); 
             $base64 = base64_encode($image); 
             return "data:image/jpeg;base64,{$base64}"; 
