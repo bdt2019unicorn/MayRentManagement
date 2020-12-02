06abbfdd97d77bdfda530bc1978db8cb119aac0f
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-12-03 01:49:41 +1300

    Update test.php

--- server/invoice_controller/test.php
+++ server/invoice_controller/test.php
@@ -9,37 +9,51 @@
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
-    $building_information = Connect::GeneralData("buildings", "5"); 
-    $building_information = $building_information[0]; 
+    // $building_information = Connect::GeneralData("buildings", "5"); 
+    // $building_information = $building_information[0]; 
 
-    $footer_array = PrintInvoices\Excel::FooterArray($building_information); 
+    // $footer_array = PrintInvoices\Excel::FooterArray($building_information); 
 
-    echo '<pre>'; 
-    print_r($building_information); 
-    print_r($footer_array); 
-    echo '</pre>'; 
+    // echo '<pre>'; 
+    // print_r($building_information); 
+    // print_r($footer_array); 
+    // echo '</pre>'; 
 
-    $html_helper = new PhpOffice\PhpSpreadsheet\Helper\Html(); 
+    // $html_helper = new PhpOffice\PhpSpreadsheet\Helper\Html(); 
 
-    $rich_text_convert = []; 
+    // $rich_text_convert = []; 
 
-    foreach ($footer_array as $row) 
+    // foreach ($footer_array as $row) 
+    // {
+    //     $rich_text_row = []; 
+    //     foreach ($row as $cell) 
+    //     {
+    //         $rich_text = $cell? $html_helper->toRichTextObject($cell): null; 
+    //         array_push($rich_text_row, $rich_text); 
+    //     }
+    //     array_push($rich_text_convert, $rich_text_row); 
+    // }
+
+
+    // $spreadsheet = new Spreadsheet(); 
+    // $sheet = $spreadsheet->getActiveSheet(); 
+    // $sheet->fromArray($rich_text_convert, null, "C3"); 
+
+    // $writer = new Xlsx($spreadsheet); 
+    // $writer->save("temp/test-footer.xlsx"); 
+
+
+    function Base64Logo()
     {
-        $rich_text_row = []; 
-        foreach ($row as $cell) 
-        {
-            $rich_text = $cell? $html_helper->toRichTextObject($cell): null; 
-            array_push($rich_text_row, $rich_text); 
-        }
-        array_push($rich_text_convert, $rich_text_row); 
+        $path = realpath(__DIR__ . "../../../img/logo.jpeg"); 
+        $image = file_get_contents($path); 
+        $base64 = base64_encode($image); 
+        return "data:image/jpeg;base64,{$base64}"; 
     }
 
-
-    $spreadsheet = new Spreadsheet(); 
-    $sheet = $spreadsheet->getActiveSheet(); 
-    $sheet->fromArray($rich_text_convert, null, "C3"); 
-
-    $writer = new Xlsx($spreadsheet); 
-    $writer->save("temp/test-footer.xlsx"); 
+    $image = Base64Logo(); 
+    $png = imagecreatefromjpeg($image); 
+    // header('Content-Type: image/jpeg');
+    imagejpeg($png); 
 
 ?>
