d21a981ea4d44c556d817fbe2f4d6082bac3ef4d
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-12-22 17:33:57 +1300

    make the document thing on the way - more tidy up need for other thing

---
+++ server/helper/OverviewQueries/Documents.php
@@ -0,0 +1,14 @@
+<?php 
+    namespace OverviewQueries; 
+    require_once("OverviewTrait.php"); 
+    class Documents 
+    {
+        use OverviewTrait 
+        {
+            Conditions as private TraitConditions; 
+        }
+
+        // private function
+    }
+
+?>
