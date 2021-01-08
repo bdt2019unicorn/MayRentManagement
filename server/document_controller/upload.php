7e1933bd688a0a44628ad18f315bee085e9e520f
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-01-08 22:19:36 +1300

    Update upload.php

--- server/document_controller/upload.php
+++ server/document_controller/upload.php
@@ -3,7 +3,23 @@
     {
         mkdir("temp"); 
     }
+    $folder = "temp/{$_POST['folder']}"; 
+    if(!file_exists($folder))
+    {
+        mkdir($folder); 
+    }
+    $file_destination = "{$folder}/{$_POST['part']}"; 
+    $file_path = "{$folder}/file"; 
+    move_uploaded_file($_FILES["blob"]["tmp_name"], $file_destination); 
 
-    echo "<pre>"; print_r($_POST); print_r($_FILES); echo '</pre>'; 
+    $file = fopen($file_destination, "rb"); 
+    $buffer = fread($file, filesize($file_destination)); 
+    fclose($file); 
+
+    $final = fopen($file_path, "ab"); 
+    $write = fwrite($final, $buffer); 
+    fclose($final); 
+
+    print_r($_POST); print_r($_FILES); 
 
 ?>
