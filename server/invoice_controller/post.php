071f800ae2fab7eacc4dc482fe570326264f5bfa
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-01-20 21:11:24 +1300

    Update post.php

--- server/invoice_controller/post.php
+++ server/invoice_controller/post.php
@@ -4,6 +4,7 @@
     (
         "AddMonthlyInvoices" => function()
         {
+            echo "here"; return; 
             $monthly_invoices = json_decode($_POST["monthly_invoices"], true); 
             $test_mode = CurrentEnvironment::TestMode(); 
             $sql = array_map(function($invoices) use ($test_mode) {return ImportInvoice($invoices, $test_mode);}, $monthly_invoices); 
