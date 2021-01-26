7c00fba8bc73c9c39e567713f7128cf1b6858f8d
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-01-26 22:39:24 +1300

    ✨feature: issue separate from the admin page

--- issues.php
+++ issues.php
@@ -1,15 +1,18 @@
 <html>
-    <?php include_once("admin/layout/1.head.php"); ?>
-    <body>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/issue.css">
+    </head>
+    <body>
+        <a href=".." class="mx-3">Main Page</a>
+        <a href="issues.php" class="mx-3">Issues</a>
         <div class="container-fluid">
             <?php 
-                require_once("../server/helper/database.php"); 
+                require_once("server/helper/database.php"); 
                 $current_environment = new CurrentEnvironment(); 
-
+                $repo = $current_environment->Repo(); 
                 $issue_id = $_GET["id"]??null; 
-                $repo = file_get_contents("repo.json"); 
-                $repo = json_decode($repo); 
                 $url = "https://api.github.com/repos/{$repo->user}/{$repo->repo}/issues"; 
             ?>
 
@@ -17,20 +20,20 @@
                 <?php 
                     $url.="/{$issue_id}"; 
                 ?>
-            <section id="issue">
-                <h1 id="showtitle"></h1>
-                <div class="issue__des container-fluid">
-                    <p id="updatecoment"></p>
-                </div>
-                <hr> <br>
-                <div id="list__comment"></div>
-                <form action="#" class="text-center" method="POST">
+                <section id="issue">
+                    <h1 id="showtitle"></h1>
                     <div class="issue__des container-fluid">
-                        <textarea placeholder="Leave a comment" name="comment" id="comment" cols="30" rows="10" class="issue__textarea form-control" required></textarea>
+                        <p id="updatecoment"></p>
                     </div>
-                    <button type="button" class="btn btn-success text-center float-right" onclick="PostComment()">Comment</button>
-                </form>
-            </section>
+                    <hr> <br>
+                    <div id="list__comment"></div>
+                    <form action="#" class="text-center" method="POST">
+                        <div class="issue__des container-fluid">
+                            <textarea placeholder="Leave a comment" name="comment" id="comment" cols="30" rows="10" class="issue__textarea form-control" required></textarea>
+                        </div>
+                        <button type="button" class="btn btn-success text-center float-right" onclick="PostComment()">Comment</button>
+                    </form>
+                </section>
             <?php elseif(isset($_GET["action"])):?>
                 <form action="#" class="issue__create text-center" method="POST">
                     <h1>Issue Ticket</h1>
@@ -69,7 +72,9 @@
             <?php endif; ?>
         </div>
         <footer>
-            <?php include_once("admin/layout/3.footer.php"); ?>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
             <script src="js/issues.js"></script>
             <script>
                 jQuery
