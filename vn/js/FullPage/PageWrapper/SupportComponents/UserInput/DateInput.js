d6642c015916a715257981c3377e0dfa3716755e
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-03-05 20:55:42 +1300

    kinda working now

--- vn/js/FullPage/PageWrapper/SupportComponents/UserInput/DateInput.js
+++ vn/js/FullPage/PageWrapper/SupportComponents/UserInput/DateInput.js
@@ -3,6 +3,18 @@ class DateInput extends UserInputComponent
     render() 
     {
         var value = this.state.value? moment(this.state.value): undefined; 
+        var input_props = 
+        {
+            name: this.props.name, 
+            className: "form-control width-full", 
+            readOnly: true 
+        }; 
+        if(!value)
+        {
+            input_props["value"] = ""; 
+        }
+        var validation_object = this.ValidationObject(); 
+        console.log(validation_object); 
         return (
             <UserInputFormControl title={this.props.title}>
                 <ReactDatetime
@@ -12,14 +24,24 @@ class DateInput extends UserInputComponent
                     closeOnSelect={true}
                     dateFormat="DD/MM/yyyy"
                     timeFormat={false}
-                    inputProps=
+                    inputProps={input_props}
+                />
+                <MaterialUI.IconButton 
+                    size="small" 
+                    className="mr-3"
+                    style=
                     {
                         {
-                            name: this.props.name, 
-                            className: "form-control width-full"
+                            width: "5%",
+                            position: "absolute",
+                            right: 0,
+                            top: "50%"
                         }
                     }
-                />
+                    onClick={()=>this.setState({value: undefined})}
+                >
+                    <MaterialUI.Icon fontSize="small">clear</MaterialUI.Icon>
+                </MaterialUI.IconButton>
             </UserInputFormControl>
 
         ); 
