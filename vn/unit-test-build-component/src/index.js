0863b0e3485928d013b5b4efb66158f298624830
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-03-06 22:27:13 +1300

    yarn thing

--- vn/unit-test-build-component/src/index.js
+++ vn/unit-test-build-component/src/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-window.ReactSelect = {Select, makeAnimated}; 
-window.TestSelect = (props)=>
-(
-    <Select {...props} />
-); 
+// import makeAnimated from 'react-select/animated';
+// window.ReactSelect = {Select, makeAnimated}; 
+// window.TestSelect = (props)=>
+// (
+//     <Select {...props} />
+// ); 
+window.ReactSelect = Select; 
