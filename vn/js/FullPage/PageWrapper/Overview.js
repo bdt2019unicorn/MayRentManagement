05f865e0021b8b5e0082c42219811eacce95f913
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-03-10 00:11:26 +1300

    Overview is able to compile thing - continue from here

--- vn/js/FullPage/PageWrapper/Overview.js
+++ vn/js/FullPage/PageWrapper/Overview.js
@@ -1,18 +1,49 @@
+class Test extends React.Component
+{
+    render() {
+        return (
+            <div style={this.props.style}>
+                I am testing {this.props.message}
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
 class Overview extends PageWrapperChildrenComponent
 {
     constructor(props)
     {
         super(props); 
         BindFunctions(this); 
+        this.state = 
+        {
+            table_data: this.TableData(this.CurrentController() || "overview")
+        }; 
     }
     render() 
     {
+
+        var component = null; 
+        try {
+            console.log(JSXTransformer); 
+            component = "<Test style={{color: 'red', fontSize: 'large'}} message='this things'>test babel</Test>"; 
+            component = JSXTransformer.transform(component); 
+            console.log(component); 
+            component = eval(component.code); 
+        }
+        catch(exception){console.log(exception); }
+        
         return (
             <div>
                 Overview
                 <pre>
                     {JSON.stringify(this.props, null, 2)}
+                    {component}
                 </pre>
+                <div>
+                    <ScrollingTable table={this.state.table_data} />
+                </div>
             </div>         
         );
     }
