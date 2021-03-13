c7addd46c3b24e77b82521d50ed866bc047435b1
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-03-13 20:21:37 +1300

    get everything for hidden columns

--- vn/js/FullPage/PageWrapper/Overview.js
+++ vn/js/FullPage/PageWrapper/Overview.js
@@ -1,56 +1,25 @@
-class Test extends React.Component
-{
-    render() {
-        return (
-            <div style={this.props.style}>
-                I am testing {this.props.message}
-                {this.props.children}
-            </div>
-        ); 
-    }
-}
-
 class Overview extends PageWrapperChildrenComponent
 {
     constructor(props)
     {
         super(props); 
         BindFunctions(this); 
+        var controller = this.CurrentController() || "overview"; 
+        var table = this.TableData(controller); 
+        var translate_url = `../server/translation/Overview/${controller}.json`; 
         this.state = 
         {
-            table_data: this.TableData(this.CurrentController() || "overview"), 
-            table_actions: this.TableActions(this.CurrentController() || "overview")
+            table_data: TranslateTable(table, translate_url), 
+            table_actions: this.TableActions(controller)
         }; 
     }
     render() 
     {
-
-        var component = null; 
-        try {
-            console.log(JSXTransformer); 
-            component = "<Test style={{color: 'red', fontSize: 'large'}} message='this things'>test babel</Test>"; 
-            component = JSXTransformer.transform(component); 
-            console.log(component); 
-            component = eval(component.code); 
-        }
-        catch(exception){console.log(exception); }
-        
         return (
-            <div>
-                Overview
-                <pre>
-                    {component}
-                </pre>
-                <div>
-                    <HyperlinkListCompile append={this.BuildingId()} html='<router-link to="leaseagrm/edit?id=144" append>Until March 31, 2021</router-link>`' />
-                </div>
-                <div>
-                    <HyperlinkListCompile html={`<router-link :to="{name: 'actions', params: {controller: 'leaseagrm', action: 'edit', building_id: 6}, query: {id: 146} }">Until 30/07/2022</router-link>`} />
-                </div>
-                <div>
-                    <ScrollingTable table={this.state.table_data} table_actions={this.state.table_actions} />
-                </div>
-            </div>         
+            <React.Fragment>
+                <h1>{_get(this.state.table_actions, "page_title") || "Tổng quát"}</h1>
+                <ScrollingTable table={this.state.table_data} table_actions={this.state.table_actions} />
+            </React.Fragment>
         );
     }
 }
