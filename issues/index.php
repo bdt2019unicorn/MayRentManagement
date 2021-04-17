1ce3b02039a06e80de86e934e0e04ef6d5ba0015
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-04-17 19:37:10 +1200

    modify issue page

--- issues/index.php
+++ issues/index.php
@@ -2,36 +2,36 @@
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/issues.css">
+        <link rel="stylesheet" type="text/css" href="../css/issues.css">
     </head>
     <body>
-        <?php require_once("issues/pages/general.php"); ?>
+        <?php require_once("pages/general.php"); ?>
         <div class="container-fluid">
             <?php 
                 if($issue_id)
                 {
-                    require_once("issues/pages/edit.php"); 
+                    require_once("pages/edit.php"); 
                 }
                 elseif(isset($_GET["action"]))
                 {
-                    require_once("issues/pages/add.php"); 
+                    require_once("pages/add.php"); 
                 }
                 else
                 {
-                    require_once("issues/pages/overview.php"); 
+                    require_once("pages/overview.php"); 
                 }
             ?>
         </div>
         <footer>
             <?php 
-                require_once("issues/scripts/general.php"); 
+                require_once("scripts/general.php"); 
                 if($issue_id)
                 {
-                    require_once("issues/scripts/edit.php"); 
+                    require_once("scripts/edit.php"); 
                 }
                 else
                 {
-                    require_once("issues/scripts/overview.php"); 
+                    require_once("scripts/overview.php"); 
                 }
             ?>
             <script>
