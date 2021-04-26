157a2c83953dc19cc4b299338a0f1363d9ca9818
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-04-26 21:25:19 +1200

    set up for basic calculations units

---
+++ vn/js/FullPage/Dashboard/BasicCalculationsUnits.js
@@ -0,0 +1,11 @@
+class BasicCalculationsUnits extends React.Component
+{
+    render()
+    {
+        return (
+            <a href="../server/controller/dashboard/backup.php" className="icon-same-line-word">
+                <MaterialUI.Icon>cloud_download</MaterialUI.Icon> Tải tập tin sao lưu
+            </a>
+        ); 
+    }
+}
