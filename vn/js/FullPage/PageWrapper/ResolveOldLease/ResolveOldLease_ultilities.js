2d39f5c6c24a291ca6590d58513d295ea9156572
khoadtd8035 <khoa.dtd8035@gmail.com>
2021-05-22 21:01:37 +0700

    Tach ham render ra, sua lai ham tinh tong

--- vn/js/FullPage/PageWrapper/ResolveOldLease/ResolveOldLease_ultilities.js
+++ vn/js/FullPage/PageWrapper/ResolveOldLease/ResolveOldLease_ultilities.js
@@ -19,7 +19,34 @@ class ResolveOldLease_ultilities extends PageWrapperChildrenComponent {
       Object.values(item)
     );
 
-    // console.log(chiTietTienIch);
+    const handleSoftDate = () => chiTietTienIch.filter(({ date }) => moment(date).isBefore(moment(endDate)));
+
+    const renderChiTietTienIch = () => {
+      return handleSoftDate().map((item, index) => {
+        return (
+          <TableRow key={index}>
+            <TableCell align="left">{item.revenue_type}</TableCell>
+            <TableCell align="right">
+              {moment(item.previous_date).format("DD-MM-YYYY")}
+            </TableCell>
+            <TableCell align="right">
+              {moment(item.date).format("DD-MM-YYYY")}
+            </TableCell>
+            <TableCell align="right">{item.price}</TableCell>
+            <TableCell align="right">{item.quantity}</TableCell>
+            <TableCell align="right">
+              {(item.quantity * item.price).toLocaleString()}
+            </TableCell>
+          </TableRow>
+        );
+      });
+    };
+
+    const handleTinhTong = ()=>{
+      return handleSoftDate().reduce((tongTien, item)=>{
+        return (tongTien += item.quantity * item.price);
+      }, 0);
+    }
 
     return (
       <TableContainer component={Paper}>
@@ -35,37 +62,14 @@ class ResolveOldLease_ultilities extends PageWrapperChildrenComponent {
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-              chiTietTienIch.filter(({date}) => moment(date).isBefore(moment(endDate))).map
-              (
-                (item, index)=>
-                (
-                  <TableRow key={index}>
-                    <TableCell align="left">{item.revenue_type}</TableCell>
-                    <TableCell align="right">
-                    {moment(item.previous_date).format("DD-MM-YYYY")}
-                    </TableCell>
-                    <TableCell align="right">
-                    {moment(item.date).format("DD-MM-YYYY")}
-                    </TableCell>
-                    <TableCell align="right">{item.price}</TableCell>
-                    <TableCell align="right">{item.quantity}</TableCell>
-                    <TableCell align="right">
-                    {(item.quantity * item.price).toLocaleString()}
-                    </TableCell>
-                  </TableRow>
-                )
-              )
-            }
+              {renderChiTietTienIch()}
           </TableBody>
           <TableFooter>
             <TableRow>
               <TableCell align="right" colSpan={6}>
                 <Typography variant="subtitle2" gutterBottom>
                   Tổng thành tiền:{" "}
-                  {(chiTietTienIch.reduce((tongTien, item) => {
-                    return (tongTien += item.quantity * item.price);
-                  }, 0)).toLocaleString()}
+                  {handleTinhTong().toLocaleString()}
                 </Typography>
               </TableCell>
             </TableRow>
