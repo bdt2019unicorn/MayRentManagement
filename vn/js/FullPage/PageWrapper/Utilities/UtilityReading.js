453140edf917207c6fe8dfc696210bec128d0cda
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-06-07 20:32:48 +1200

    we don't need the table as well - all merged - keep on the work

--- vn/js/FullPage/PageWrapper/Utilities/UtilityReading.js
+++ vn/js/FullPage/PageWrapper/Utilities/UtilityReading.js
@@ -8,11 +8,6 @@ class UtilityReading extends Utilities
 			...this.state,
 			unit_id: "", 
 			utilities_readings: []
-	      // mangCanHo: this.state.select_data.units,
-	      // chonCanHo: "",
-	      // danhSachTienIch: this.state.select_data.utilities,
-	      // tenCanHoChon: "",
-	      // mangThongSoTienIch: [],
 		};
 	}
 
@@ -71,13 +66,43 @@ class UtilityReading extends Utilities
 
 	render() 
 	{
-		const { Box, FormControl, Typography } = MaterialUI;
-		const { mangCanHo, chonCanHo, danhSachTienIch, mangThongSoTienIch, tenCanHoChon } = this.state;
+		const { Box, FormControl, Icon, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography, withStyles } = MaterialUI;
+  		const StyleText = withStyles
+  		(
+  			{
+  				root: 
+  				{
+			  		"& input::-webkit-clear-button, & input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button":
+			  		{
+			  			display: "none",
+			  			margin: 80
+			  		},
+			  		"&$disabled": 
+			  		{
+			  			"&:before": 
+			  			{
+			  				borderBottom: "none!important"
+			  			},
+			  			"& svg": 
+			  			{
+			  				display: "none"
+			  			} 
+  					},
+			  		underline: 
+			  		{
+			  			"&:after": 
+			  			{
+			  				transition: "none"
+			  			},
+  					} 
+  				} 
+  			}
+  		)(TextField);
 
 		return (
 			<Box>
 				<Typography variant="h4" gutterBottom>Tiện Ích</Typography>
-				<Typography variant="h6" gutterBottom>Thêm mới tiện ích</Typography>
+				<Typography variant="h6" gutterBottom>Thêm số liệu tiện ích</Typography>
 				<FormControl variant="outlined" fullWidth={true} size="small" margin="normal">
 					<div className="pr-3">
 						<SelectInput 
@@ -86,19 +111,47 @@ class UtilityReading extends Utilities
 							ValueChange={this.UnitIdValueChanged}
 						/>
 					</div>
-					{/*
-						<UtilityReadingListChoice
-						mangCanHo={mangCanHo}
-						handleChoice={this.handleChoice}
-						/>
-
-						<UtilityReadingTable
-						chonCanHo={chonCanHo}
-						danhSachTienIch={danhSachTienIch}
-						mangThongSoTienIch={mangThongSoTienIch}
-						tenCanHoChon={tenCanHoChon}
-						/>
-					*/}
+					{
+						Boolean(this.state.unit_id) && 
+						(
+							<TableContainer component={Paper}>
+    							<Table aria-label="simple table">
+    								<TableHead>
+							    		<TableRow>
+							    			<TableCell>Dịch vụ</TableCell>
+										    <TableCell>Tên căn hộ</TableCell>
+										    <TableCell>Ngày</TableCell>
+										    <TableCell>Thời gian</TableCell>
+										    <TableCell>Chỉ số ghi nhận</TableCell>
+										    <TableCell>Chỉ số trước</TableCell>
+										    <TableCell>Ngày ghi nhận chỉ số trước</TableCell>
+										    <TableCell>Xóa</TableCell>
+							    		</TableRow>
+						    		</TableHead>
+						    		<TableBody>
+									    {
+									    	this.state.utilities_readings.map
+									    	(
+									    		(utilities_reading, index) => 
+									    		(
+									        		<TableRow key={index}>
+												        <TableCell>{utilities_reading.service}</TableCell>
+												        <TableCell>{utilities_reading.unit_name}</TableCell>
+									        			<TableCell><StyleText error type="text" defaultValue={DateReformat.Display()} /></TableCell>
+									        			<TableCell><StyleText error type="text" defaultValue={DateReformat.TimeDisplay()} /></TableCell>
+									        			<TableCell><StyleText error type="number" /></TableCell>
+												        <TableCell>{utilities_reading.current_reading}</TableCell>
+												        <TableCell>{DateReformat.Display(utilities_reading.current_date)}</TableCell>
+											          <TableCell><IconButton><Icon>delete</Icon></IconButton></TableCell>
+								          			</TableRow>
+									          	) 
+									      	)
+									  	}
+							      	</TableBody>
+						      	</Table>
+					      	</TableContainer>
+						)
+					}
 				</FormControl>
 				<SubmitButton type="button" />
 			</Box>
