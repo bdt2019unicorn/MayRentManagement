c31206f634c663359935fb02df249a21b209e670
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-06-11 02:59:01 +1200

    may need to cherry pick this if you need for the user information things to be sent through

--- vn/js/functions/support.js
+++ vn/js/functions/support.js
@@ -3,6 +3,7 @@ function AjaxRequest(url, data = new FormData(), type = "get")
     var result = undefined; 
     const request = new XMLHttpRequest(); 
     request.open(type, url, false); 
+    UserInformation.Header(request); 
     request.onload = ()=> result = request.responseText; 
     request.send(data); 
     return result; 
@@ -11,10 +12,10 @@ function BlobRequest(url, data={})
 {
     var result = undefined; 
     const request = new XMLHttpRequest(); 
-    
     request.overrideMimeType("text/plain; charset=x-user-defined"); 
     request.open("POST", url, false); 
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    UserInformation.Header(request); 
     request.onload = ()=>
     {
         var bytes = new Uint8Array(request.responseText.length);
@@ -56,18 +57,9 @@ function SubmitData(key, url, data, stringify=true)
 {
     var form_data = new FormData(); 
     form_data.append(key, (stringify)?JSON.stringify(data): data); 
-    SubmitUserInformation(form_data); 
+    UserInformation.Submit(form_data); 
     return AjaxRequest(url, form_data, "post"); 
 }
-function SubmitUserInformation(form_data)
-{
-    try 
-    {
-        form_data.append("username", sessionStorage.getItem("username")); 
-        form_data.append("modified_time", moment().format("YYYY-MM-DD HH:MM:ss")); 
-    }
-    catch(error) {}
-} 
 function TableAction(controller)
 {
     return ServerJson(`../server/json/table_actions/vn/${controller}.json`) || {}; 
