d6492b4283f5d4b7920e0fd7cc4fc131eaadca72
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-06-11 02:59:01 +1200

    may need to cherry pick this if you need for the user information things to be sent through

--- vn/js/functions/SupportGroups.js
+++ vn/js/functions/SupportGroups.js
@@ -11,3 +11,18 @@ class DateReformat
     static Database = (string = undefined) => this.MomentDate(string).format("YYYY-MM-DD") 
     static Display = (string = undefined) => this.MomentDate(string).format("DD/MM/YYYY")
 }
+
+class UserInformation 
+{
+    static ExecFunctionUserInformation(func)
+    {
+        try 
+        {
+            func("username", sessionStorage.getItem("username")); 
+            func("modified_time", moment().format("YYYY-MM-DD HH:MM:ss")); 
+        }
+        catch {}
+    }
+    static Header = (request) => this.ExecFunctionUserInformation(request.setRequestHeader.bind(request))
+    static Submit = (form_data) => this.ExecFunctionUserInformation(form_data.append.bind(form_data))
+}
