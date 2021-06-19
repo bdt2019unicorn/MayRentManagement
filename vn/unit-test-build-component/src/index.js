b576b892a228802f01516b4ffa74b7abe2ec46f0
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2021-06-19 16:49:33 +1200

    this things fails too

--- vn/unit-test-build-component/src/index.js
+++ vn/unit-test-build-component/src/index.js
@@ -1,2 +1,6 @@
-import DateRangePicker from '@wojtekmaj/react-daterange-picker';
-window.DateRangePicker = DateRangePicker; 
+import InfiniteCalendar, {
+    Calendar,
+    withRange,
+  } from 'react-infinite-calendar';
+  import 'react-infinite-calendar/styles.css';
+  window.ReactInfinateCalendar = {InfiniteCalendar, Calendar, withRange}; 
